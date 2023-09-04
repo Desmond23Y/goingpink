ab28edd15b5b952e176ef72776c177cbc5badfa3
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-05 00:47:08 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,23 +1,29 @@
-<?php
-?>
-
+<!DOCTYPE html>
 <html>
-    <head>
-        <style>
-            .footer {
-  color: #black;
-  background-color: #950070;
-  text-align: center;
-  padding: 10px 0;
-  position: absolute;
-  width: 100%;
+<head>
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    min-height: 100vh; 
+    position: relative;
 }
-        </style>
-    </head>
-    <body>
-        <div class="footer">
-        <p>&copy; 2023 GoingPink</p>
-        </div>
 
-    </body>
+.footer {
+    color: #333;
+    background-color: #F9B9C3;
+    text-align: center;
+    padding: 10px 0;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+}
+</style>
+</head>
+    
+<body>
+    <div class="footer">
+        <p>&copy; 2023 GoingPink</p>
+    </div>
+</body>
 </html>
