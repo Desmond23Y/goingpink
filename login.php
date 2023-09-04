736bf6d769cf78e6e71745dbb2a8cbbdbcf8852e
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-04 23:50:54 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,13 @@
+<?php
+include('navi_bar.php')
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travel Service Management</title>
+    <title>Login</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
