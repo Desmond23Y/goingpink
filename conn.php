74dd8e647e236db941ad1d5abf78653da0b042b0
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-04 16:29:59 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","id21215862_goingpink","@Goingpink123","id1215862_goingpink");
+$con = mysqli_connect("localhost","id21215862_goingpink","@Goingpink123","id21215862_goingpink");
 
 if ($con->connect_error) {
     die("Could not connect to the server!: " . $con->connect_error);
