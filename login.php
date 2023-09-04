5c8e00625dc84c13014bcc7d01faeff835c909b2
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-05 03:24:53 +0800

    Update login.php

--- login.php
+++ login.php
@@ -16,10 +16,10 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $password = validate($_POST['password']);
 
     if (empty($username)) {
-        header("Location: login.php?error=Username is required");
+        echo "error: Username is required";
         exit();
     } else if (empty($password)) {
-        header("Location: login.php?error=Password is required");
+        echo "error: Password is required";
         exit();
     } else {
         
@@ -38,12 +38,12 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
                 exit();
             } else {
 
-                header("Location: login.php?error=Incorrect password");
+                echo "error: Incorrect password";
                 exit();
             }
         } else {
             
-            header("Location: login.php?error=Database error");
+            echo "error: Database error";
             exit();
         }
     }
