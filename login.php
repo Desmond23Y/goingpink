4a9248b0ba1b06f2254a1986aa4d05858b1062d9
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-05 02:33:01 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,45 @@
 <?php
-include('navi_bar.php');
-include('conn.php');
+include('navi_bar.php')
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Service Management</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome Back, Traveller!</h1>
+        <h3>"Embrace the Journey, Go Pink with Excellence!"</h3>
+    </header>
+    <div id="login-container">
+        <h2>Login</h2>
+        <form id="login-form">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <button type="submit">Login</button>
+        </form>
+        <p id="login-message"></p>
+        <button id="logout-btn" style="display: none;">Logout</button>
+    </div>
+
+    
+</body>
+</html>
+
+<?php
+include('conn.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-    $sql = "SELECT password_hash FROM users WHERE username = ?";
-
+    
+    $sql = "SELECT password FROM users WHERE username = ?";
+    
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "s", $username);
     mysqli_stmt_execute($stmt);
@@ -17,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (password_verify($password, $hashed_password)) {
         // Password is correct, redirect the user to the dashboard or homepage
-        header("Location: dashboard.php"); // Replace with your actual destination
+        header("Location: index.php"); // Replace with your actual destination
         exit();
     } else {
         // Failed login, display an error message
