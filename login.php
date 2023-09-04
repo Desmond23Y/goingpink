59343c57f16f2da1c79054c1fe3577c31030d7d6
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-05 03:19:37 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,53 @@
 <?php
-include('navi_bar.php')
+session_start();
+include('navi_bar.php');
+include('conn.php');
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+
+    function validate($data){
+       $data = trim($data);
+       $data = stripslashes($data);
+       $data = htmlspecialchars($data);
+       return $data;
+    }
+
+    $username = validate($_POST['username']);
+    $password = validate($_POST['password']);
+
+    if (empty($username)) {
+        header("Location: login.php?error=Username is required");
+        exit();
+    } else if (empty($password)) {
+        header("Location: login.php?error=Password is required");
+        exit();
+    } else {
+        
+        $sql = "SELECT * FROM user WHERE username='$username'";
+        $result = mysqli_query($con, $sql);
+
+        if ($result) {
+            $row = mysqli_fetch_assoc($result);
+            if (password_verify($password, $row['password'])) {
+
+
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['name'] = $row['name'];
+                $_SESSION['id'] = $row['id'];
+                header("Location: index.php");
+                exit();
+            } else {
+
+                header("Location: login.php?error=Incorrect password");
+                exit();
+            }
+        } else {
+            
+            header("Location: login.php?error=Database error");
+            exit();
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -31,51 +79,3 @@ include('navi_bar.php')
     
 </body>
 </html>
-
-<?php
-include('conn.php');
-if (isset($_POST['username']) && isset($_POST['password'])) {
-
-    function validate($data){
-       $data = trim($data);
-       $data = stripslashes($data);
-       $data = htmlspecialchars($data);
-       return $data;
-    }
-
-    $username = validate($_POST['username']);
-    $password = validate($_POST['password']);
-
-    if (empty($username)) {
-        header("Location: login.php");
-        echo "Error: Username is required";
-        exit();
-
-    }else if(empty($password)){
-        header("Location: login.php");
-        echo "Error: Password is required";
-        exit();
-        
-    }else{
-        $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
-        $result = mysqli_query($con, $sql);
-        if (mysqli_num_rows($result) === 1) {
-
-            $row = mysqli_fetch_assoc($result);
-            if ($row['username'] === $username && $row['password'] === $pass) {
-                echo "Logged in!";
-
-                $_SESSION['username'] = $row['username'];
-                $_SESSION['name'] = $row['name'];
-                $_SESSION['id'] = $row['id'];
-                header("Location: index.php");
-                exit();
-
-            }else{
-                header("Location: login.php");
-                echo "Error: Incorrect Username or Password";
-                exit();
-            }else{
-            header("Location: index.php");
-            exit();
-        }
