3eefc5da9a3504964529191902ea00de014404ee
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-04 15:07:43 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,38 @@
 <?php
+include('navbar.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Service Management</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome Back, Traveller!</h1>
+        <h3>"Embrace the Journey, Go Pink with Excellence!"</h3>
+    </header>
+    <div id="login-container">
+        <h2>Login</h2>
+        <form id="login-form">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <button type="submit">Login</button>
+        </form>
+        <p id="login-message"></p>
+        <button id="logout-btn" style="display: none;">Logout</button>
+    </div>
+
+    
+</body>
+</html>
+
+<?php
 include('conn.php'); 
 
 function login($username, $password) {
@@ -62,35 +96,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     login($username, $password);
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travel Service Management</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <header>
-        <h1>Welcome Back, Traveller!</h1>
-        <h3>"Embrace the Journey, Go Pink with Excellence!"</h3>
-    </header>
-    <div id="login-container">
-        <h2>Login</h2>
-        <form id="login-form">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required><br><br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
-            <button type="submit">Login</button>
-        </form>
-        <p id="login-message"></p>
-        <button id="logout-btn" style="display: none;">Logout</button>
-    </div>
-
-    
-</body>
-</html>
-
 
