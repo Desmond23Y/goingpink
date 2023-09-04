66be2b4ec53f83484c9ca31f1a9ef0d5ebe1685d
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-04 14:26:18 +0800

    Add files via upload

---
+++ Ticket.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<head>
+    <title>Customer Support Request Form</title>
+    <link rel="stylesheet" href="Style Ticket.css">
+</head>
+
+<body>
+    <header>
+        <h1>Submit request form for customer support</h1>
+    </header>
+
+    <section class="request-form">
+        <h2>Help Request Form</h2>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            include("conn.php");
+
+            $sql = "INSERT INTO ticket (contact_name, support_type, ticket_description) VALUES ('$_POST[name]', '$_POST[support]', '$_POST[description]')";
+
+            if(!mysqli_query($con,$sql)) {
+                die('Error:' . mysqli_error($con));
+            }
+            else {
+                echo "<script>alert('This form has been submitted!');</script>";
+            }
+
+            mysqli_close($con);
+        }
+        ?>
+        <form id="request-form" method="post">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required="required"><br>
+
+            <label for="support type">Choose a support type:</label>
+            <select name="support" id="support" required="required">
+                <option value="">Please select a support type</option>
+                <option value="Technical">Technical</option>
+                <option value="Payment and Billing Support">Payment and Billing Support</option>
+                <option value="Feedback and Complaint Resolution">Feedback and Complaint</option>
+                <option value="Others">Others</option>
+            </select><br>
+
+            <label for="description">Description:</label>
+            <textarea id="text" name="description" rows="5" cols="50" required="required"></textarea><br>
+
+            <button type="submit" value="Submit">Submit Form</button>
+            
+        </form><br>
+
+    </section>
+</body>
+</html>
---
+++ View Booking.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Going Pink Hotel Viewing</title>
+        <link rel="stylesheet" href="Style Booking.css">
+
+    </head>
+    <body>
+    <header>
+        <h1>Hotel Viewing</h1>
+    </header>
+
+        <?php include("conn.php");
+        $result=mysqli_query($con,"SELECT* FROM hotel_info");
+        while($row=mysqli_fetch_array($result))
+        {
+        ?>
+
+    <div class="box">
+        <?php
+            echo '<h3> Hotel ID: '. $row["hotel_id"].'</h3>';
+            echo '<h3> Hotel Name: '. $row["hotel_name"].'</h3>';
+            echo '<h3> Room Type: '. $row["room_type"].'</h3>';
+            echo '<h3> Room Availability: '. $row["room_availability"].'</h3>';
+            echo '<h3> Hotel Availability: '. $row["hotel_availability"].'</h3>';
+            echo '<h3> Hotel Price: '. $row["hotel_price"].'</h3>';
+        }
+        ?>
+    </div>
+
+    </body>
+</html>
