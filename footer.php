a8a415568cadce0bc76738d091bfde66752dcb61
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-05 00:58:41 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@ body {
     color: #333;
     background-color: #F9B9C3;
     text-align: center;
-    padding: 14px 16px;
+    padding: 10px 0px;
     position: absolute;
     width: 100%;
     bottom: 0;
