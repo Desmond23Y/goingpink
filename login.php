1c3964315e1692b946606f6aa7c95c86cb966b6f
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-04 15:13:28 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,46 +1,12 @@
 <?php
-include('navbar.php')
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travel Service Management</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <header>
-        <h1>Welcome Back, Traveller!</h1>
-        <h3>"Embrace the Journey, Go Pink with Excellence!"</h3>
-    </header>
-    <div id="login-container">
-        <h2>Login</h2>
-        <form id="login-form">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required><br><br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
-            <button type="submit">Login</button>
-        </form>
-        <p id="login-message"></p>
-        <button id="logout-btn" style="display: none;">Logout</button>
-    </div>
-
-    
-</body>
-</html>
-
-<?php
-include('conn.php'); 
+include('conn.php');
 
 function login($username, $password) {
-    global $conn;
+    global $con;
 
     
-    $username = ($conn, $username);
-    $password = ($conn, $password);
+    $username = ($con, $username);
+    $password = ($con, $password);
 
     // Query all relevant tables using a UNION query
     $query = "SELECT 'user' AS user_type, user_id FROM user WHERE username = '$username' AND password = '$password'
@@ -53,7 +19,7 @@ function login($username, $password) {
               UNION
               SELECT 'hotel_management' AS user_type, hotel_manager_id FROM hotel_management WHERE username = '$username' AND password = '$password'";
 
-    $result = mysqli_query($conn, $query);
+    $result = mysqli_query($con, $query);
 
     if ($result && mysqli_num_rows($result) === 1) {
         // Fetch the user type and user_id
@@ -90,10 +56,38 @@ function login($username, $password) {
 
 // Verify login form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username']; 
-    $password = $_POST['password']; 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
     login($username, $password);
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Service Management</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome Back, Traveller!</h1>
+        <h3>"Embrace the Journey, Go Pink with Excellence!"</h3>
+    </header>
+    <div id="login-container">
+        <h2>Login</h2>
+        <!-- Use the POST method for the form -->
+        <form id="login-form" method="POST" action="">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <button type="submit">Login</button>
+        </form>
+        <p id="login-message"></p>
+        <button id="logout-btn" style="display: none;">Logout</button>
+    </div>
+</body>
+</html>
