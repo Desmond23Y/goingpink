10203c97ed0019670c05c24573872fb8bf17b71a
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-05 13:49:15 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,53 +1,33 @@
 <?php
-session_start();
 include('navi_bar.php');
 include('conn.php');
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-    function validate($data){
-       $data = trim($data);
-       $data = stripslashes($data);
-       $data = htmlspecialchars($data);
-       return $data;
-    }
-
-    $username = validate($_POST['username']);
-    $password = validate($_POST['password']);
-
-    if (empty($username)) {
-        echo "error: Username is required";
-        exit();
-    } else if (empty($password)) {
-        echo "error: Password is required";
-        exit();
+    // Validate user inputs
+    if (empty($username) || empty($password)) {
+        echo "Username and password are required.";
     } else {
-        
-        $sql = "SELECT * FROM user WHERE username='$username'";
-        $result = mysqli_query($con, $sql);
+        // Prepare and execute the SQL query to check if the user exists
+        $check_user_query = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+        $result = mysqli_query($con, $check_user_query);
 
-        if ($result) {
-            $row = mysqli_fetch_assoc($result);
-            if (password_verify($password, $row['password'])) {
-
-
-                $_SESSION['username'] = $row['username'];
-                $_SESSION['name'] = $row['name'];
-                $_SESSION['id'] = $row['id'];
-                header("Location: index.php");
-                exit();
-            } else {
-
-                echo "error: Incorrect password";
-                exit();
-            }
-        } else {
-            
-            echo "error: Database error";
+        if (mysqli_num_rows($result) > 0) {
+            // Login successful
+            echo "<script>alert('Login successful!');</script>";
+            // Redirect to a dashboard or profile page
+            header('Location: dashboard.php');
             exit();
+        } else {
+            // Login failed
+            echo "Invalid username or password. Please try again.";
         }
     }
 }
+
+mysqli_close($con);
 ?>
 
 <!DOCTYPE html>
@@ -60,22 +40,22 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 </head>
 <body>
     <header>
-        <h1>Welcome Back, Traveller!</h1>
-        <h3>"Embrace the Journey, Go Pink with Excellence!"</h3>
+        <h1>Welcome to Going Pink!</h1>
+        <h3>"The only Travel System Service you ever need!"</h3>
     </header>
     <div id="login-container">
         <h2>Login</h2>
-        <form id="login-form" method="POST" action="login.php">
+        <form id="login-form" method="POST" action="">
             <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required><br><br>
+            <input type="text" id="username" name="username" maxlength="50" required><br><br>
+
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
+            <input type="password" id="password" name="password" maxlength="50" required><br><br>
+
             <button type="submit">Login</button>
         </form>
         <p id="login-message"></p>
         <button id="logout-btn" style="display: none;">Logout</button>
     </div>
-
-    
 </body>
 </html>
