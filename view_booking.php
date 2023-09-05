2a5cce672744a6ee2f32bfd8d4552a7cb7aaa857
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-05 08:20:06 +0800

    Update view_booking.php

--- view_booking.php
+++ view_booking.php
@@ -1,40 +1,44 @@
 <?php
 include('navi_bar.php')
+include("conn.php");
+
+$result = mysqli_query($con, "SELECT * FROM hotel_information");
+if (!$result) {
+    die('Query Error: ' . mysqli_error($con));
+}
+
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Going Pink Hotel Viewing</title>
-        <link rel="stylesheet" href="style_booking.css">
-
-    </head>
-    <body>
+<head>
+    <title>Going Pink Hotel Viewing</title>
+    <link rel="stylesheet" href="style_booking.css">
+</head>
+<body>
     <header>
         <h1>Hotel Viewing</h1>
     </header>
 
-        <?php include("conn.php");
-        $result=mysqli_query($con,"SELECT* FROM hotel_information");
-        while($row=mysqli_fetch_array($result))
-        {
-        ?>
-
     <div class="box">
         <?php
-            echo '<h3> Hotel ID: '. $row["hotel_id"].'</h3>';
-            echo '<h3> Hotel Name: '. $row["hotel_name"].'</h3>';
-            echo '<h3> Room Type: '. $row["room_type"].'</h3>';
-            echo '<h3> Room Availability: '. $row["room_availability"].'</h3>';
-            echo '<h3> Hotel Availability: '. $row["hotel_availability"].'</h3>';
-            echo '<h3> Hotel Price: '. $row["hotel_price"].'</h3>';
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<h3> Hotel ID: '. $row["hotel_id"].'</h3>';
+                echo '<h3> Hotel Name: '. $row["hotel_name"].'</h3>';
+                echo '<h3> Room Type: '. $row["room_type"].'</h3>';
+                echo '<h3> Room Availability: '. $row["room_availability"].'</h3>';
+                echo '<h3> Hotel Availability: '. $row["hotel_availability"].'</h3>';
+                echo '<h3> Hotel Price: US$ '. $row["hotel_price"].'</h3>';
+            }
+        } else {
+            echo "No hotels available.";
         }
         ?>
     </div>
 
-    </body>
+</body>
 </html>
-
 <?php
 include('footer.php');
 ?>
