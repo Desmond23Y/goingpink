c5691de9bb016d046dc2788f43022c07fc7d4c21
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-05 14:54:11 +0800

    Update edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -1,8 +1,18 @@
 <?php
+// Start or resume the session
+session_start();
+
+include('navi_bar.php');
 include('conn.php');
 
-// Assuming you have a way to retrieve the user's ID
-$user_id = 123; // Replace with the actual user ID
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php'); // Redirect to the login page if not logged in
+    exit();
+}
+
+// Get the user ID from the session
+$user_id = $_SESSION['user_id'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Process the form data
@@ -17,31 +27,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $confirm_password = $_POST['confirmPassword'];
 
     // Check if the username is already taken
-    $check_username_query = "SELECT * FROM user WHERE username = '$username'";
+    $check_username_query = "SELECT * FROM user WHERE username = '$username' AND id != $user_id";
     $result = mysqli_query($con, $check_username_query);
 
     // Perform user input validation
-    if (strlen($username) < 5 || strlen($username) > 50) {
-        echo "Length of username must be between 5 and 50.";
-    } elseif (mysqli_num_rows($result) > 0) {
-        echo "Username already exists. Please try again.";  
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email address. Please try again.";
-    } elseif (!strpos($email, "@") || !strpos($email, ".com")) {
-        echo "Email address must contain '@' and '.com'. Please try again.";
-    } elseif (strlen($phone_number) > 9 || strlen($phone_number) < 12) {
-        echo "Invalid phone number. Please try again.";
-    } elseif (strlen($new_password) > 10 || strlen($new_password) < 50) {
-        echo "Password length must be between 5 and 50 characters. Please try again.";
-    } elseif (!preg_match('/[A-Z]/', $new_password)) {
-        echo "Password must contain at least one UPPERCASE letter. Please try again.";
-    } elseif (!preg_match('/[a-z]/', $new_password)) {
-        echo "Password must contain at least one lowercase letter. Please try again.";
-    } elseif (!preg_match('/[^a-zA-Z0-9]/', $new_password)) {
-        echo "Password must contain at least one special character. Please try again.";
-    } elseif ($new_password !== $confirm_password) {
-        echo "Password confirmation does not match. Please try again.";
-    } else {
+    // (rest of the validation code)
+
+    if (/* All validation checks pass */) {
         // Update the user information in the database
         $update_profile_query = "UPDATE user SET 
             username = '$username',
@@ -64,12 +56,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 } else {
-   $errormessage = "Invalid request. ";
+   $errormessage = "Invalid request.";
 }
-?>
-<?php
+
+// Fetch the existing user data
+$fetch_user_query = "SELECT * FROM user WHERE id = $user_id";
+$result = mysqli_query($con, $fetch_user_query);
+
+if ($result && mysqli_num_rows($result) > 0) {
+    $user_data = mysqli_fetch_assoc($result);
+
+    // Populate form fields with existing data
+    $username = $user_data['username'];
+    $first_name = $user_data['first_name'];
+    $last_name = $user_data['last_name'];
+    $email = $user_data['email'];
+    $phone_number = $user_data['phone_number'];
+    $date_of_birth = $user_data['date_of_birth'];
+    $gender = $user_data['gender'];
+
+    // Display the form with existing data
+} else {
+    echo "Error fetching user data: " . mysqli_error($con);
+}
+
 include('navi_bar.php');
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -81,55 +94,17 @@ include('navi_bar.php');
 <body>
     <h1>Edit Profile</h1>
     <div class="container">
-    <form action="update_profile.php" method="POST">
+    <form action="profile_edit.php" method="POST">
         <!-- User Information -->
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username">
+        <input type="text" id="username" name="username" value="<?php echo $username; ?>">
         <br><br>
 
         <label for="first_name">First Name:</label>
-        <input type="text" id="first_name" name="first_name">
+        <input type="text" id="first_name" name="first_name" value="<?php echo $first_name; ?>">
         <br><br>
 
-        <label for="lastName">Last Name:</label>
-        <input type="text" id="last_name" name="last_name">
-        <br><br>
-
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email">
-        <br><br>
-
-        <label for="phone_number">Phone Number:</label>
-        <input type="tel" id="phone_number" name="phonenumber">
-        <br><br>
-
-        <label for="date_of_birth">Date of Birth:</label>
-        <input type="date" id="date_of_birth" name="date_of_birth">
-        <br><br>
-
-        <label>Gender:</label><br>
-        <input type="radio" id="male" name="gender" value="male">
-        <label for="male">Male</label><br>
-
-        <input type="radio" id="female" name="gender" value="female">
-        <label for="female">Female</label><br>
-
-        <input type="radio" id="other" name="gender" value="other">
-        <label for="other">Other</label>
-        <br><br>
-
-        <!-- Password Change -->
-        <label for="currentPassword">Current Password:</label>
-        <input type="password" id="currentPassword" name="currentPassword">
-        <br><br>
-
-        <label for="newPassword">New Password:</label>
-        <input type="password" id="newPassword" name="newPassword">
-        <br><br>
-
-        <label for="confirmPassword">Confirm New Password:</label>
-        <input type="password" id="confirmPassword" name="confirmPassword">
-        <br><br>
+        <!-- Rest of the form fields -->
 
         <!-- Submit Button -->
         <input type="submit" value="Save Changes">
