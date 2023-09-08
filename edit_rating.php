1cbd446983ccec14af414a5a47e204912a12a2c3
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-08 22:35:50 +0800

    Update edit_rating.php

--- edit_rating.php
+++ edit_rating.php
@@ -8,27 +8,36 @@ include('conn.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $new_rating = $_POST['new_rating'];
     $new_feedback = $_POST['new_feedback'];
-    $rating_id = $_POST['rating_id'];
 
     // Validate user inputs
     if (empty($new_rating) || empty($new_feedback)) {
         echo "<script>alert('Rating and feedback are required.');</script>";
     } else {
+        // Get user information from the session
+        $user_type = $_SESSION['user_type'];
+        $user_id = $_SESSION['user_id'];
+
         // Update the rating and feedback in the "rating" table
-        $update_query = "UPDATE rating SET total_stars_rating = '$new_rating', feedback_description = '$new_feedback' WHERE id = '$rating_id'";
+        $update_query = "UPDATE rating SET total_stars_rating = '$new_rating', feedback_description = '$new_feedback' WHERE user_id = '$user_id'";
 
         if (mysqli_query($con, $update_query)) {
             echo "<script>alert('Rating and feedback updated successfully!');</script>";
         } else {
-            echo "Error: " . mysqli_error($con);
+            echo "Error updating rating and feedback: " . mysqli_error($con);
         }
     }
 }
 
-// Retrieve the user's existing ratings and feedback
+// Retrieve the user's existing rating and feedback
 $user_id = $_SESSION['user_id'];
-$select_query = "SELECT id, total_stars_rating, feedback_description FROM rating WHERE user_id = '$user_id'";
+$select_query = "SELECT total_stars_rating, feedback_description FROM rating WHERE user_id = '$user_id'";
 $result = mysqli_query($con, $select_query);
+
+if ($result) {
+    $row = mysqli_fetch_assoc($result);
+} else {
+    echo "Error retrieving rating and feedback: " . mysqli_error($con);
+}
 
 // Close the database connection
 mysqli_close($con);
@@ -47,18 +56,16 @@ mysqli_close($con);
         <h1>Edit Rating and Feedback</h1>
     </header>
     <div id="edit-rating-container">
-        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-            <form method="POST" action="">
-                <input type="hidden" name="rating_id" value="<?php echo $row['id']; ?>">
-                <label for="new_rating">Rating (1-5):</label>
-                <input type="number" id="new_rating" name="new_rating" min="1" max="5" value="<?php echo $row['total_stars_rating']; ?>" required><br><br>
+        <h2>Edit Your Rating and Feedback</h2>
+        <form id="edit-rating-form" method="POST" action="">
+            <label for="new_rating">New Rating (1-5):</label>
+            <input type="number" id="new_rating" name="new_rating" min="1" max="5" value="<?php echo isset($row['total_stars_rating']) ? $row['total_stars_rating'] : ''; ?>" required><br><br>
 
-                <label for="new_feedback">Feedback:</label>
-                <textarea id="new_feedback" name="new_feedback" rows="4" cols="50" required><?php echo $row['feedback_description']; ?></textarea><br><br>
+            <label for="new_feedback">New Feedback:</label>
+            <textarea id="new_feedback" name="new_feedback" rows="4" cols="50" required><?php echo isset($row['feedback_description']) ? $row['feedback_description'] : ''; ?></textarea><br><br>
 
-                <button type="submit">Update</button>
-            </form>
-        <?php } ?>
+            <button type="submit">Update</button>
+        </form>
     </div>
 </body>
 </html>
