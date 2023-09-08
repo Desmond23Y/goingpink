f5d49f64262f4c8168fe137c6ff179f141a48803
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-08 22:11:00 +0800

    Create edit_rating.php

---
+++ edit_rating.php
@@ -0,0 +1,66 @@
+<?php
+// Start or resume the session
+session_start();
+
+include('navi_bar.php');
+include('conn.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $new_rating = $_POST['new_rating'];
+    $new_feedback = $_POST['new_feedback'];
+
+    // Validate user inputs
+    if (empty($new_rating) || empty($new_feedback)) {
+        echo "<script>alert('Rating and feedback are required.');</script>";
+    } else {
+        // Get user information from the session
+        $user_type = $_SESSION['user_type'];
+        $user_id = $_SESSION['user_id'];
+
+        // Update the rating and feedback in the "rating" table
+        $update_query = "UPDATE rating SET total_stars_rating = '$new_rating', feedback_description = '$new_feedback' WHERE user_id = '$user_id'";
+
+        if (mysqli_query($con, $update_query)) {
+            echo "<script>alert('Rating and feedback updated successfully!');</script>";
+        } else {
+            echo "Error: " . mysqli_error($con);
+        }
+    }
+}
+
+// Retrieve the user's existing rating and feedback
+$user_id = $_SESSION['user_id'];
+$select_query = "SELECT total_stars_rating, feedback_description FROM rating WHERE user_id = '$user_id'";
+$result = mysqli_query($con, $select_query);
+$row = mysqli_fetch_assoc($result);
+
+// Close the database connection
+mysqli_close($con);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Rating and Feedback</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Edit Rating and Feedback</h1>
+    </header>
+    <div id="edit-rating-container">
+        <h2>Edit Your Rating and Feedback</h2>
+        <form id="edit-rating-form" method="POST" action="">
+            <label for="new_rating">New Rating (1-5):</label>
+            <input type="number" id="new_rating" name="new_rating" min="1" max="5" value="<?php echo $row['total_stars_rating']; ?>" required><br><br>
+
+            <label for="new_feedback">New Feedback:</label>
+            <textarea id="new_feedback" name="new_feedback" rows="4" cols="50" required><?php echo $row['feedback_description']; ?></textarea><br><br>
+
+            <button type="submit">Update</button>
+        </form>
+    </div>
+</body>
+</html>
