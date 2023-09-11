315e28691061d1338dab00bee2bc560647b7be34
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-11 23:13:27 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -24,38 +24,3 @@ include('navi_bar.php'); // Moved the inclusion of 'navi_bar.php' after the pote
 mysqli_close($con);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travel Service Management</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <header>
-        <h1>Welcome to Going Pink!</h1>
-        <h3>"The only Travel System Service you ever need!"</h3>
-    </header>
-    <div id="login-container">
-        <h2>Login</h2>
-        <form id="login-form" method="POST" action="">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" maxlength="50" required><br><br>
-
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" maxlength="50" required><br><br>
-
-            <input type="checkbox" id="remember_me" name="remember_me">
-            <label for="remember_me">Remember Me</label><br><br>
-
-            <button type="submit">Login</button>
-        </form>
-        <form method="POST" action="">
-            <button type="submit" name="logout">Logout</button>
-        </form>
-        <p id="login-message"></p>
-        <button id="logout-btn" style="display: none;">Logout</button>
-    </div>
-</body>
-</html>
