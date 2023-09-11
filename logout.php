80fdc8ebc440c11c96cb1fc45d06a846d388e3dd
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-11 23:22:59 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['user_type']) && isset($_SESSION['user_id'])) {
     echo "<script>alert('Please login first!');</script>";
 }
 
-include('index.php'); // Moved the inclusion of 'navi_bar.php' after the potential error messages
+include('navi_bar.php'); // Moved the inclusion of 'navi_bar.php' after the potential error messages
 
 
 mysqli_close($con);
