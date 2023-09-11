6096fca0f33b7e6d8592bfe6e123956cd9bea779
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-12 00:06:42 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,62 +4,57 @@ session_start();
 
 include('conn.php');
 
-// Check if the user is already logged in
-if (isset($_SESSION['user_type']) && isset($_SESSION['user_id'])) {
-    // Display a "Logout" button
-    echo '<a href="logout.php">Logout</a>';
-} else {
-    // User is not logged in; process the login form
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-        // Validate user inputs
-        if (empty($username) || empty($password)) {
-            echo "Username and password are required.";
-        } else {
-            // Prepare and execute the UNION query to check if the user exists in any table
-            $union_query = "
-                (SELECT 'admin' AS user_type, admin_id AS user_id FROM admin WHERE username = '$username' AND password = '$password')
-                UNION
-                (SELECT 'support' AS user_type, support_id AS user_id FROM support WHERE username = '$username' AND password = '$password')
-                UNION
-                (SELECT 'hotel_management' AS user_type, hotel_manager_id AS user_id FROM hotel_management WHERE username = '$username' AND password = '$password')
-                UNION
-                (SELECT 'transport_management' AS user_type, transport_manager_id AS user_id FROM transport_management WHERE username = '$username' AND password = '$password')
-                UNION
-                (SELECT 'user' AS user_type, user_id AS user_id FROM user WHERE username = '$username' AND password = '$password')
-            ";
+    // Validate user inputs
+    if (empty($username) || empty($password)) {
+        echo "Username and password are required.";
+    } else {
+        // Prepare and execute the UNION query to check if the user exists in any table
+        $union_query = "
+            (SELECT 'admin' AS user_type, admin_id AS user_id FROM admin WHERE username = '$username' AND password = '$password')
+            UNION
+            (SELECT 'support' AS user_type, support_id AS user_id FROM support WHERE username = '$username' AND password = '$password')
+            UNION
+            (SELECT 'hotel_management' AS user_type, hotel_manager_id AS user_id FROM hotel_management WHERE username = '$username' AND password = '$password')
+            UNION
+            (SELECT 'transport_management' AS user_type, transport_manager_id AS user_id FROM transport_management WHERE username = '$username' AND password = '$password')
+            UNION
+            (SELECT 'user' AS user_type, user_id AS user_id FROM user WHERE username = '$username' AND password = '$password')
+        ";
 
-            $result = mysqli_query($con, $union_query);
+        $result = mysqli_query($con, $union_query);
 
-            if (mysqli_num_rows($result) > 0) {
-                // Login successful
-                $row = mysqli_fetch_assoc($result);
-                $user_type = $row['user_type'];
-                $user_id = $row['user_id'];
+        if (mysqli_num_rows($result) > 0) {
+            // Login successful
+            $row = mysqli_fetch_assoc($result);
+            $user_type = $row['user_type'];
+            $user_id = $row['user_id'];
 
-                // Store user data in the session
-                $_SESSION['user_type'] = $user_type;
-                $_SESSION['user_id'] = $user_id;
+            // Store user data in the session
+            $_SESSION['user_type'] = $user_type;
+            $_SESSION['user_id'] = $user_id;
+     
 
-                // Check if the "Remember Me" checkbox is checked
-                if (isset($_POST['remember_me'])) {
-                    // Create a JSON string with the username and password and set it as a cookie
-                    $cookie_data = json_encode(['username' => $username, 'password' => $password]);
-                    setcookie('remember_me', $cookie_data, time() + (60 * 60 * 24 * 30)); // Cookie will expire in 30 days
-                }
-                // Redirect to index.php
-                header('Location: index.php');
-                exit();
-            } else {
-                // Login failed
-                echo "Invalid username or password. Please try again.";
+            // Check if the "Remember Me" checkbox is checked
+            if (isset($_POST['remember_me'])) {
+                // Create a JSON string with the username and password and set it as a cookie
+                $cookie_data = json_encode(['username' => $username, 'password' => $password]);
+                setcookie('remember_me', $cookie_data, time() + (60 * 60 * 24 * 30)); // Cookie will expire in 30 days
             }
+            // Redirect to index.php
+            header('Location: index.php');
+            exit();
+        } else {
+            // Login failed
+            echo "Invalid username or password. Please try again.";
         }
     }
-    mysqli_close($con);
 }
+
+mysqli_close($con);
 ?>
 
 <?php
@@ -98,4 +93,3 @@ include_once('navi_bar.php');
     </div>
 </body>
 </html>
-
