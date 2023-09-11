9692113e5a83d761a81397475f2cfb1ce7363b0a
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-11 16:20:21 +0800

    Update login.php

--- login.php
+++ login.php
@@ -5,6 +5,43 @@ session_start();
 include('navi_bar.php');
 include('conn.php');
 
+// Check if the "Remember Me" cookie is set
+if (isset($_COOKIE['remember_me'])) {
+    $cookie_data = json_decode($_COOKIE['remember_me'], true);
+
+    $username = $cookie_data['username'];
+    $password = $cookie_data['password'];
+
+    // Attempt to log in using the provided credentials
+    $union_query = "
+        (SELECT 'admin' AS user_type, admin_id AS user_id FROM admin WHERE username = '$username' AND password = '$password')
+        UNION
+        (SELECT 'support' AS user_type, support_id AS user_id FROM support WHERE username = '$username' AND password = '$password')
+        UNION
+        (SELECT 'hotel_management' AS user_type, hotel_manager_id AS user_id FROM hotel_management WHERE username = '$username' AND password = '$password')
+        UNION
+        (SELECT 'transport_management' AS user_type, transport_manager_id AS user_id FROM transport_management WHERE username = '$username' AND password = '$password')
+        UNION
+        (SELECT 'user' AS user_type, user_id AS user_id FROM user WHERE username = '$username' AND password = '$password')
+    ";
+
+    $result = mysqli_query($con, $union_query);
+
+    if (mysqli_num_rows($result) > 0) {
+        // Login successful
+        $row = mysqli_fetch_assoc($result);
+        $user_type = $row['user_type'];
+        $user_id = $row['user_id'];
+
+        // Store user data in the session
+        $_SESSION['user_type'] = $user_type;
+        $_SESSION['user_id'] = $user_id;
+
+        echo "<script>alert('Login successful as $user_type with ID $user_id!');</script>";
+        exit();
+    }
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -37,8 +74,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Store user data in the session
             $_SESSION['user_type'] = $user_type;
             $_SESSION['user_id'] = $user_id;
-            header('Location: index.php');
-            
+
+            // Check if the "Remember Me" checkbox is checked
+            if (isset($_POST['remember_me'])) {
+                // Create a JSON string with the username and password and set it as a cookie
+                $cookie_data = json_encode(['username' => $username, 'password' => $password]);
+                setcookie('remember_me', $cookie_data, time() + (60 * 60 * 24 * 30)); // Cookie will expire in 30 days
+            }
 
             echo "<script>alert('Login successful as $user_type with ID $user_id!');</script>";
             exit();
@@ -73,6 +115,9 @@ mysqli_close($con);
 
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" maxlength="50" required><br><br>
+
+            <input type="checkbox" id="remember_me" name="remember_me">
+            <label for="remember_me">Remember Me</label><br><br>
 
             <button type="submit">Login</button>
         </form>
