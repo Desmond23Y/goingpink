fff5f8ee3e69fbb89f09109695a39845afa65b92
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-11 18:50:28 +0800

    Update navi_bar.php

--- navi_bar.php
+++ navi_bar.php
@@ -41,11 +41,11 @@ body {
                 <li><a href="view_booking.php">BOOKING</a></li>
                 <li><a href="api_exRates.php">EXCHANGE RATES</a></li>
                 <li><a href="request_form.php">SUPPORT</a></li>
+                <li><a href="view_rating.php">VIEW RATING</a></li>
+                <li><a href="rating.php">LEAVE A RATING</a></li>
+                <li><a href="view_profile.php" class="right">PROFILE</a></li>
                 <li><a href="login.php" class="right">LOGIN</a></li>
                 <li><a href="register.php" class="right">REGISTER</a></li>
-                <li><a href="edit_profile.php" class="right">PROFILE</a></li>
-                <li><a href="rating.php" class="right">LEAVE A RATING</a></li>
-                <li><a href="view_rating.php" class="right">VIEW RATING</a></li>
                 <li><a href="logout.php" class="right">LOGOUT</a></li>
             </ul>
         </nav>
