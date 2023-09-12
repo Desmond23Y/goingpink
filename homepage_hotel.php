57511377d1271eee56f7679df457ccc44ba6e31e
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-12 11:54:17 +0800

    Update homepage_hotel.php

--- homepage_hotel.php
+++ homepage_hotel.php
@@ -1,1 +1,16 @@
+<?php
 
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Hotel Management Homepage</title>
+    <link rel="stylesheet" href="homepage_hote_styles.css">
+</head>
+<body>
+<h1> Hotel Management Homepage </h1>
+  
+</body>
+</html>
