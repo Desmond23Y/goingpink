2f6d225390c13d7a1f0507d0b5ec06d00a521d85
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-12 13:42:14 +0800

    Update hotelbooking.php

--- hotelbooking.php
+++ hotelbooking.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('conn.php'); // Include your database connection script
+include('conn.php'); 
 $user_id = $_SESSION['user_id'];
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['number_of_pax']) && isset($_POST['check_in_date']) && isset($_POST['check_out_date'])) {
     $number_of_pax = $_POST['number_of_pax'];
