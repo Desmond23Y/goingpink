a315284a2a3a956d72c44f1dc4f6fed269b4cc63
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-12 23:43:16 +0800

    Update transportM_homepage.php

--- transportM_homepage.php
+++ transportM_homepage.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Transport Management Homepage</title>
-    <link rel="stylesheet" href="transportM_styles.css">
+    <link rel="stylesheet" href="M_navibar.css">
     <nav>
         <ul class="navibar">
             <li><a href="index.php">HOME</a></li>
