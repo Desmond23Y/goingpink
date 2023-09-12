60b47fb037a0518c3f009d03a18d7685f4d528f2
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-12 14:50:38 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,24 @@
+<?php
+// Start or resume the session
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    // User is logged in, log them out
+    include('conn.php'); 
+    if (isset($_POST['logout'])) {
+        // Unset session variables
+        unset($_SESSION['user_type']);
+        unset($_SESSION['user_id']);
+
+        // Destroy the session
+        session_destroy();
+
+        echo "<script>alert('Logged out successfully!');</script>";
+    }
+} else {
+    // User is not logged in, redirect to the login page
+    header('Location: login.php'); 
+    exit();
+}
+?>
 
