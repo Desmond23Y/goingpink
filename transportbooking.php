137879301c9311747bddf5251fec5946066157cf
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-12 23:08:52 +0800

    Update transportbooking.php

--- transportbooking.php
+++ transportbooking.php
@@ -5,36 +5,41 @@ include('conn.php');
 $user_id = $_SESSION['user_id'];
 $selected_transport_id = $_SESSION['selected_transport_id'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['number_of_tickets']) && isset($_POST['departure_date'])) {
-    $number_of_tickets = $_POST['number_of_tickets'];
+// Define departure based on transport type
+$departure_location = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['departure_date'])) {
     $departure_date = $_POST['departure_date'];
 
-    // Validate user inputs
-    if ($number_of_tickets < 1) {
-        echo "Number of tickets must be at least 1.";
-    } elseif (strtotime($departure_date) < strtotime(date('Y-m-d'))) {
-        echo "Invalid departure date.";
-    } else {
-        // Check if the selected transport ID exists in the transport_information table
-        $check_transport_query = "SELECT * FROM transport_information WHERE transport_id = '$selected_transport_id'";
-        $result = mysqli_query($con, $check_transport_query);
+    // Get the selected transport type from the database
+    $get_transport_type_query = "SELECT transport_type FROM transport_information WHERE transport_id = '$selected_transport_id'";
+    $result = mysqli_query($con, $get_transport_type_query);
 
-        if (mysqli_num_rows($result) == 0) {
-            echo "Selected transport not found.";
+    if (!$result || mysqli_num_rows($result) == 0) {
+        echo "Selected transport not found.";
+    } else {
+        $row = mysqli_fetch_assoc($result);
+        $transport_type = $row['transport_type'];
+
+        // Set departure based on the transport type
+        if ($transport_type == 'Aeroplane') {
+            $departure_location = 'KLIA2';
         } else {
-            // Insert the transport booking
-            $transport_booking_query = "INSERT INTO transport_booking (user_id, transport_id, departure_date) 
-                                    VALUES ('$user_id', '$selected_transport_id', '$departure_date')";
-            
-            if (mysqli_query($con, $transport_booking_query)) {
-                header('Location: viewtransport.php');
-            } else {
-                echo "Error: " . mysqli_error($con);
-            }
+            $departure_location = 'Bandar Tasik Selatan';
         }
 
-        mysqli_close($con);
+        // Insert the transport booking with departure
+        $transport_booking_query = "INSERT INTO transport_booking (user_id, transport_id, departure_date, departure_location) 
+                                VALUES ('$user_id', '$selected_transport_id', '$departure_date', '$departure_location')";
+        
+        if (mysqli_query($con, $transport_booking_query)) {
+            header('Location: viewtransport.php');
+        } else {
+            echo "Error: " . mysqli_error($con);
+        }
     }
+
+    mysqli_close($con);
 }
 ?>
 
@@ -46,7 +51,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['number_of_tickets']) 
 <body>
     <h2>Book a Transport</h2>
     <form id="transport-booking-form" method="POST" action="">
-
         <label for="departure_date">Departure Date:</label>
         <input type="date" id="departure_date" name="departure_date" required><br><br>
 
@@ -55,4 +59,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['number_of_tickets']) 
     <p id="booking-message"></p>
 </body>
 </html>
-
