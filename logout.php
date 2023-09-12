55447b00c3fbe5ef63bfb3a76c796f752bd6d48e
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-12 15:07:54 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -14,6 +14,7 @@ if (isset($_SESSION['user_id'])) {
         session_destroy();
 
         echo "<script>alert('Logged out successfully!');</script>";
+        header('Location: login.php'); 
     }
 } else {
     // User is not logged in, redirect to the login page
