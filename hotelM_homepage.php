7eedfc9e8dff462635acce9c372317a3a9e5bb27
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-12 23:15:44 +0800

    Update and rename homepage_hotel.php to hotelM_homepage.php

--- hotelM_homepage.php
+++ hotelM_homepage.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Hotel Management Homepage</title>
-    <link rel="stylesheet" href="homepage_hotel_styles.css">
+    <link rel="stylesheet" href="hotelM_styles.css">
 <style>
 body {
     margin: 0;
