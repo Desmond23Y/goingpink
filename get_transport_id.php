84a6a8144f3a4ab9b73074dd9675fffe8cc6a561
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-13 23:40:34 +0800

    Update get_transport_id.php

--- get_transport_id.php
+++ get_transport_id.php
@@ -1,11 +1,11 @@
 <?php
-include('conn.php');
+include('conn.php'); // Include your database connection file
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve POST data
+    // Retrieve the selected transport_type from the AJAX request
     $transportType = $_POST['transport_type'];
 
-    // Query the transport_information table to retrieve the transport_id
+    // Query to retrieve the transport_id based on transport_type
     $query = "SELECT transport_id FROM transport_information WHERE transport_type = ?";
     $stmt = mysqli_prepare($con, $query);
 
@@ -13,13 +13,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         mysqli_stmt_bind_param($stmt, "s", $transportType);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_bind_result($stmt, $transportId);
+        mysqli_stmt_fetch($stmt);
 
-        if (mysqli_stmt_fetch($stmt)) {
+        if ($transportId) {
+            // Return the transport_id as a JSON response
             echo json_encode(['transport_id' => $transportId]);
-            exit();
+        } else {
+            echo json_encode(['transport_id' => null]);
         }
-    }
-}
 
-echo json_encode(['transport_id' => null]);
+        mysqli_stmt_close($stmt);
+    } else {
+        echo json_encode(['transport_id' => null]);
+    }
+} else {
+    echo json_encode(['transport_id' => null]);
+}
 ?>
