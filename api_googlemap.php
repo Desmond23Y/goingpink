31cef6950bd86457e33c0b7ffa56366787f8db23
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-09-13 22:07:28 +0800

    Update api_googlemap.php

--- api_googlemap.php
+++ api_googlemap.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Distance Calculator</title>
+    <title>Transport Bookingr</title>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBS7LY-BaBKUS0xIRTNJKXtfsLEZv_5OG8&libraries=places&callback=initAutocomplete" async defer></script>
 </head>
 
