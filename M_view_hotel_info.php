2ec38628fa14ef0ce9e59126b524e61bdd39fb89
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-17 21:29:08 +0800

    Update and rename M_view_hotel_info to M_view_hotel_info.php

---
+++ M_view_hotel_info.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Information</title>
+    <link rel="stylesheet" href="M_navibar.css">
+    <nav>
+        <ul class="navibar">
+            <li><a href="M_hotel_homepage.php">HOME</a></li>
+            <li><a href="M_view_hotel_info.php">HOTELS</a></li>
+            <li><a href="M_viewbooking.php">BOOKING</a></li>
+            <li><a href="M_room_availability.php">ROOM AVAILABILITY</a></li>
+            <li><a href="logout.php" class="right">LOGOUT</a></li>
+        </ul>
+    </nav>
+</head>
+
+<body>
+<h1>Hotel Information</h1>
+</body>
+</html>
