3766fe1222f62d5d8b32689db03d32384128d82f
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-17 21:30:13 +0800

    Update M_hotel_homepage.php

--- M_hotel_homepage.php
+++ M_hotel_homepage.php
@@ -6,20 +6,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Hotel Management Homepage</title>
+    <title> Hotel Management's Homepage</title>
     <link rel="stylesheet" href="M_navibar.css">
     <nav>
         <ul class="navibar">
-            <li><a href="homepage_hotel.php">HOME</a></li>
-            <li><a href="view_hotel_info.php">HOTELS</a></li>
-            <li><a href="viewbooking.php">BOOKING</a></li>
-            <li><a href="room_availability.php">ROOM AVAILABILITY</a></li>
+            <li><a href="M_hotel_homepage.php">HOME</a></li>
+            <li><a href="M_view_hotel_info.php">HOTELS</a></li>
+            <li><a href="M_viewbooking.php">BOOKING</a></li>
+            <li><a href="M_room_availability.php">ROOM AVAILABILITY</a></li>
             <li><a href="logout.php" class="right">LOGOUT</a></li>
         </ul>
     </nav>
 </head>
 
 <body>
-<h1> Hotel Management Homepage </h1>
+<h1>Hotel Management's Homepage</h1>
 </body>
 </html>
