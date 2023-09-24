138f91c48f9317f53d29b5062f2dddea7c55505a
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-24 14:12:34 +0800

    Add files via upload

---
+++ create_hmgt_acc.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include("conn.php");
+    if (strlen($_POST['name']) > 5 && strlen($_POST['name']) < 50) {
+        echo "Length of username must be between 5 and 50 characters.";
+    } elseif (strlen($_POST['password']) > 5 && strlen($_POST['password']) < 50) {
+        echo "Password length must be between 5 and 50 characters. Please try again.";
+    } else
+        $sql = "INSERT INTO hotel_management (username, password) VALUES ('$_POST[name]', '$_POST[password]')";
+
+    if(!mysqli_query($con,$sql)) {
+        die('Error:' . mysqli_error($con));
+    }
+    else {
+        echo "<script>alert('This hotel management account has been created!');</script>";
+    }
+
+    mysqli_close($con);
+}
+?>
+
+<!DOCTYPE html>
+<body>
+    <form id="request-form" method="post">
+        <label for="name">Username:</label>
+        <input type="text" id="name" name="name" required="required"><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" maxlength="50" required><br><br>
+
+        <button type="submit">Register as Hotel Management</button>
+    </form>
+</body>
+</html>
+
