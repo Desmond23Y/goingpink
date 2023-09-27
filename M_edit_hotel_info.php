38951566191c70cedc2fa618e9ce8057f74916fa
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-28 02:46:36 +0800

    latest

--- M_edit_hotel_info.php
+++ M_edit_hotel_info.php
@@ -23,6 +23,26 @@ if (isset($_GET['hotel_id'])) {
     echo "Hotel ID not provided.";
     exit();
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve form data
+    $newHotelName = $_POST['hotelName'];
+    $newRoomType = $_POST['roomType'];
+    $newHotelAvailability = $_POST['hotelAvailability'];
+    $newHotelPrice = $_POST['hotelPrice'];
+
+    // Update the hotel information in the database
+    $updateQuery = "UPDATE hotel_information SET hotel_name = '$newHotelName', room_type = '$newRoomType', hotel_availability = $newHotelAvailability, hotel_price = '$newHotelPrice' WHERE hotel_id = $hotelID";
+    $updateResult = mysqli_query($con, $updateQuery);
+
+    if ($updateResult) {
+        // Redirect back to the hotel view page or show a success message
+        header('Location: M_view_hotel_info.php');
+        exit();
+    } else {
+        echo 'Error updating hotel information: ' . mysqli_error($con);
+    }
+}
 ?>
 
 <html>
@@ -47,40 +67,17 @@ if (isset($_GET['hotel_id'])) {
 <div class="box">
 <form action="M_edit_hotel_info.php" method="post">
         <label for="hotelName">Hotel Name: </label>
-        <input type="text" id="hotelName" name="hotelName" required>
+        <input type="text" id="hotelName" name="hotelName" required value="<?php echo $hotelName; ?>">
     <br><br>
         <label for="roomType">Room Type: </label>
-        <input type="text" id="roomType" name="roomType" required>
+        <input type="text" id="roomType" name="roomType" required value="<?php echo $roomType; ?>">
     <br><br>
         <label for="hotelAvailability">Hotel Availability: </label>
-        <input type="number" id="hotelAvailability" name="hotelAvailability" required>
+        <input type="number" id="hotelAvailability" name="hotelAvailability" required value="<?php echo $hotelAvailability; ?>">
     <br><br>
         <label for="hotelPrice">Hotel Price: </label>
-        <input type="text" id="hotelPrice" name="hotelPrice" required>
+        <input type="text" id="hotelPrice" name="hotelPrice" required value="<?php echo $hotelPrice; ?>">
         <br><br>
         <input type="submit" value="Save Changes">
 </form>
 </div>
-
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve form data
-    $newHotelName = $_POST['hotelName'];
-    $newRoomType = $_POST['roomType'];
-    $newHotelAvailability = $_POST['hotelAvailability'];
-    $newHotelPrice = $_POST['hotelPrice'];
-
-    // Update the hotel information in the database
-    $updateQuery = "UPDATE hotel_information SET hotel_name = '$newHotelName', room_type = '$newRoomType', hotel_availability = $newHotelAvailability, hotel_price = '$newHotelPrice' WHERE hotel_id = $hotelID";
-    $updateResult = mysqli_query($con, $updateQuery);
-
-    if ($updateResult) {
-        // Redirect back to the hotel view page or show a success message
-        header('Location: M_view_hotel_info.php');
-        exit();
-    } else {
-        echo 'Error updating hotel information: ' . mysqli_error($con);
-    }
-}
-?>
