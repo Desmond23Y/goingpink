2492b7b848f515aa479d908efa70ce32eaa129f5
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 02:51:17 +0800

    Update M_edit_hotel_info.php

--- M_edit_hotel_info.php
+++ M_edit_hotel_info.php
@@ -28,6 +28,51 @@ if (isset($_GET['hotel_id'])) {
                 hotel_availability = ?,
                 hotel_price = ?
                 WHERE hotel_id = ?";
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Edit Hotel Information</title>
+                <link rel="stylesheet" href="M_navibar.css">
+                 <nav>
+                    <ul class="navibar">
+                        <li><a href="M_hotel_homepage.php">HOME</a></li>
+                        <li><a href="M_view_hotel_info.php">HOTELS INFO</a></li>
+                        <li><a href="M_viewbooking.php">BOOKING</a></li>
+            
+                        <li><a href="logout.php" class="right">LOGOUT</a></li>
+                    </ul>
+                </nav>
+            </head>
+              
+            <body>
+                <h1>Edit Hotel Information</h1>
+                <div class="box">
+                    <form action="M_edit_hotel_info.php?hotel_id=<?php echo $hotel_id; ?>" method="POST">
+                        <label for="hotel_id">Hotel ID: </label>
+                        <input name="hotel_id" readonly="readonly" value="<?php echo $hotel_id; ?>">
+                        <br><br>
+                        <label for="hotel_name">Hotel Name: </label>
+                        <input type="text" id="hotel_name" name="hotel_name" required value="<?php echo $row['hotel_name']; ?>">
+                        <br><br>
+                        <label for="room_type">Room Type: </label>
+                        <input type="text" id="room_type" name="room_type" required value="<?php echo $row['room_type']; ?>">
+                        <br><br>
+                        <label for="room_availability">Room Availability: </label>
+                        <input type="number" id="room_availability" name="room_availability" required value="<?php echo $row['room_availability']; ?>">
+                        <br><br>
+                        <label for="hotel_availability">Hotel Availability: </label>
+                        <input type="text" id="hotel_availability" name="hotel_availability" required value="<?php echo $row['hotel_availability']; ?>">
+                        <br><br>
+                        <label for="hotel_price">Hotel Price: </label>
+                        <input type="number" id="hotel_price" name="hotel_price" required value="<?php echo $row['hotel_price']; ?>">
+                        <br><br>
+                        <input type="submit" name="submit" value="Save Changes">
+                    </form>
+                </div>
+            </body>
+            </html>
+
 
             $update_stmt = mysqli_prepare($con, $update_sql);
             mysqli_stmt_bind_param($update_stmt, "ssssi", $hotel_name, $room_type, $hotel_availability, $hotel_price, $hotel_id);
@@ -41,48 +86,3 @@ if (isset($_GET['hotel_id'])) {
     }
 }
 ?>
-        
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Hotel Information</title>
-    <link rel="stylesheet" href="M_navibar.css">
-     <nav>
-        <ul class="navibar">
-            <li><a href="M_hotel_homepage.php">HOME</a></li>
-            <li><a href="M_view_hotel_info.php">HOTELS INFO</a></li>
-            <li><a href="M_viewbooking.php">BOOKING</a></li>
-
-            <li><a href="logout.php" class="right">LOGOUT</a></li>
-        </ul>
-    </nav>
-</head>
-  
-<body>
-    <h1>Edit Hotel Information</h1>
-    <div class="box">
-        <form action="M_edit_hotel_info.php?hotel_id=<?php echo $hotel_id; ?>" method="POST">
-            <label for="hotel_id">Hotel ID: </label>
-            <input name="hotel_id" readonly="readonly" value="<?php echo $hotel_id; ?>">
-            <br><br>
-            <label for="hotel_name">Hotel Name: </label>
-            <input type="text" id="hotel_name" name="hotel_name" required value="<?php echo $row['hotel_name']; ?>">
-            <br><br>
-            <label for="room_type">Room Type: </label>
-            <input type="text" id="room_type" name="room_type" required value="<?php echo $row['room_type']; ?>">
-            <br><br>
-            <label for="room_availability">Room Availability: </label>
-            <input type="number" id="room_availability" name="room_availability" required value="<?php echo $row['room_availability']; ?>">
-            <br><br>
-            <label for="hotel_availability">Hotel Availability: </label>
-            <input type="text" id="hotel_availability" name="hotel_availability" required value="<?php echo $row['hotel_availability']; ?>">
-            <br><br>
-            <label for="hotel_price">Hotel Price: </label>
-            <input type="number" id="hotel_price" name="hotel_price" required value="<?php echo $row['hotel_price']; ?>">
-            <br><br>
-            <input type="submit" name="submit" value="Save Changes">
-        </form>
-    </div>
-</body>
-</html>
