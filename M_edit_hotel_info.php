8980c090c755f4bbe190d9ea2cb35d13310497a7
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-28 21:38:45 +0800

    10

--- M_edit_hotel_info.php
+++ M_edit_hotel_info.php
@@ -65,7 +65,7 @@ if (isset($_GET['hotel_id'])) {
 <body>
     <h1>Edit Hotel Information</h1>
     <div class="box">
-        <form action="M_edit_hotel_info.php" method="POST">
+        <form action= method="POST">
         <input type="hidden" name="hotel_id" value="<?php echo $hotel_id; ?>">
             <label for="hotelName">Hotel Name: </label>
             <input type="text" id="hotelName" name="hotelName" required value="<?php echo $hotelName; ?>">
