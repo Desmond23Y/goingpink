2eb780c5f0861bd14f08065bbbc330d76fddc4c7
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 03:12:11 +0800

    Update M_edit_hotel_info.php

--- M_edit_hotel_info.php
+++ M_edit_hotel_info.php
@@ -49,7 +49,7 @@ if (isset($_GET['hotel_id'])) {
                     <ul class="navibar">
                         <li><a href="M_hotel_homepage.php">HOME</a></li>
                         <li><a href="M_view_hotel_info.php">HOTELS INFO</a></li>
-                        <li><a href="M_viewbooking.php">BOOKING</a></li>
+                        <li><a href="M_view_hotel_booking.php">BOOKING</a></li>
             
                         <li><a href="logout.php" class="right">LOGOUT</a></li>
                     </ul>
