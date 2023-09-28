5f125d15dee368004317119c810539f981777e3d
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 01:03:37 +0800

    Update update_ticket.php

--- update_ticket.php
+++ update_ticket.php
@@ -7,81 +7,97 @@
     <h2>Ticket Update</h2>
     <?php
         include("conn.php");
-	if (isset($_GET['ticket_id'])) {
-		$ticket_id=intval($_GET['ticket_id']); 
-		$result=mysqli_query($con,"SELECT* FROM ticket WHERE ticket_id=$ticket_id");
-		while($row=mysqli_fetch_array($result))
-		{
-		$sql="UPDATE ticket SET
-		ticket_status = '$_POST[status]',
-		ticket_priority = '$_POST[priority]',
-		ticket_solution = '$_POST[solution]'
-	
-		WHERE ticket_id = $_POST[ticket_id];";
-	}
-
-        if (mysqli_query($con,$sql)) {
-            mysqli_close($con);
-        }
-
+        if (isset($_GET['ticket_id'])) {
+            $ticket_id = intval($_GET['ticket_id']); 
+            $result = mysqli_query($con, "SELECT * FROM ticket WHERE ticket_id = $ticket_id");
+            
+            while ($row = mysqli_fetch_array($result)) {
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    // Handle form submission and update database here
+                    $ticket_id = $row['ticket_id']; // Get the ticket_id from the row
+                    $status = $_POST['status'];
+                    $priority = $_POST['priority'];
+                    $solution = $_POST['solution'];
+                    
+                    // Use prepared statements to prevent SQL injection
+                    $update_sql = "UPDATE ticket SET
+                        ticket_status = ?,
+                        ticket_priority = ?,
+                        ticket_solution = ?
+                        WHERE ticket_id = ?";
+                    
+                    $update_stmt = mysqli_prepare($con, $update_sql);
+                    mysqli_stmt_bind_param($update_stmt, "sssi", $status, $priority, $solution, $ticket_id);
+                    
+                    if (mysqli_stmt_execute($update_stmt)) {
+                        echo "<script>alert('Ticket information has been updated!');</script>";
+                    } else {
+                        echo "Error updating ticket information: " . mysqli_error($con);
+                    }
+                }
+                
+                // Display ticket information and form
     ?>
-        <p>
-        <label> Ticket ID: </label>
-		<input name='ticket_id' readonly='readonly' value="<?php echo $row['ticket_id']?>">
-		</p>
-		
-		<p>
-        <label> Support ID: </label>
-		<input name='sid' readonly='readonly' value="<?php echo $row['support_id']?>">
-		</p>
-		
-		<p>
-        <label> User ID: </label>
-		<input name='uid' readonly='readonly' value="<?php echo $row['user_id']?>">
-		</p>
-		
-		<p>
-        <label> Contact Name: </label>
-		<input name='name' readonly='readonly' value="<?php echo $row['contact_name']?>">
-		</p>
+                <p>
+                    <label> Ticket ID: </label>
+                    <input name='ticket_id' readonly='readonly' value="<?php echo $row['ticket_id']?>">
+                </p>
+                
+                <p>
+                    <label> Support ID: </label>
+                    <input name='sid' readonly='readonly' value="<?php echo $row['support_id']?>">
+                </p>
+                
+                <p>
+                    <label> User ID: </label>
+                    <input name='uid' readonly='readonly' value="<?php echo $row['user_id']?>">
+                </p>
+                
+                <p>
+                    <label> Contact Name: </label>
+                    <input name='name' readonly='readonly' value="<?php echo $row['contact_name']?>">
+                </p>
 
-        <p>
-        <label> Support Type: </label>
-		<input name='typt' readonly='readonly' value="<?php echo $row['support_type']?>">
-		</p>
+                <p>
+                    <label> Support Type: </label>
+                    <input name='typt' readonly='readonly' value="<?php echo $row['support_type']?>">
+                </p>
 
-        <p>
-        <label> Ticket Description: </label>
-		<input name='desc' readonly='readonly' value="<?php echo $row['ticket_description']?>">
-		</p>
+                <p>
+                    <label> Ticket Description: </label>
+                    <input name='desc' readonly='readonly' value="<?php echo $row['ticket_description']?>">
+                </p>
 
-        <p>
-        <form method="post">
-            <label for="status">Choose the Ticket Status:</label>
-            <select name="status" id="status" required="required">
-                <option value="">Please select the ticket status</option>
-                <option value="Created">Created</option>
-                <option value="Pending">Pending</option>
-                <option value="Resolved">Resolved</option>
-                <option value="Closed">Closed</option>
-            </select><br>
+                <form method="post">
+                    <label for="status">Choose the Ticket Status:</label>
+                    <select name="status" id="status" required="required">
+                        <option value="">Please select the ticket status</option>
+                        <option value="Created">Created</option>
+                        <option value="Pending">Pending</option>
+                        <option value="Resolved">Resolved</option>
+                        <option value="Closed">Closed</option>
+                    </select><br>
 
-            <label for="priority">Choose the Ticket Priority:</label>
-            <select name="priority" id="priority" required="required">
-                <option value="">Please select the level of the priority</option>
-                <option value="High">High</option>
-                <option value="Medium">Medium</option>
-                <option value="Low">Low</option>
-            </select><br>
+                    <label for="priority">Choose the Ticket Priority:</label>
+                    <select name="priority" id="priority" required="required">
+                        <option value="">Please select the level of the priority</option>
+                        <option value="High">High</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Low">Low</option>
+                    </select><br>
 
-            <label for="solution">Solution for this Problem:</label>
-            <textarea id="text" name="solution" rows="5" cols="50" required="required"></textarea><br>
+                    <label for="solution">Solution for this Problem:</label>
+                    <textarea id="text" name="solution" rows="5" cols="50" required="required"></textarea><br>
 
-        <button type="submit" value="Submit">Update Ticket</button>
-        
-    </form><br>
+                    <button type="submit" value="Submit">Update Ticket</button>
+                </form><br>
     <?php
-    }mysqli_close($con);
+            }
+            mysqli_close($con);
+        } else {
+            echo "Ticket ID not provided.";
+        }
     ?>
 </body>
 </html>
+
