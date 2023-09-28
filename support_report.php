d6d9d8d940096b837d343aeb349329d1da67c5a8
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-28 22:09:14 +0800

    Update support_report.php

--- support_report.php
+++ support_report.php
@@ -1,7 +1,27 @@
 <?php
 session_start();
-?>
 
+if (!isset($_SESSION['support_id'])) {
+    header('Location: login.php'); // Redirect to the login page if not logged in
+    exit();
+}
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $support_id = $_SESSION['support_id'];
+    include("conn.php");
+
+    $sql = "INSERT INTO report (support_id, report_title, priority, report_description, report_status) VALUES ('$support_id', '$_POST[title]', '$_POST[priority]', '$_POST[description]', 'Created')";
+
+    if(!mysqli_query($con,$sql)) {
+        die('Error:' . mysqli_error($con));
+    }
+    else {
+        echo "<script>alert('This form has been submitted!');</script>";
+    }
+
+    mysqli_close($con);
+}
+?>
+        
 <!DOCTYPE html>
 <head>
     <title>Report to Admin</title>
@@ -11,28 +31,7 @@ session_start();
     <header>
         <h1>Report Form</h1>
     </header>
-
-    <?php
-    if (!isset($_SESSION['support_id'])) {
-        header('Location: login.php'); // Redirect to the login page if not logged in
-        exit();
-    }
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $support_id = $_SESSION['support_id'];
-        include("conn.php");
-
-        $sql = "INSERT INTO report (support_id, report_title, priority, report_description, report_status) VALUES ('$support_id', '$_POST[title]', '$_POST[priority]', '$_POST[description]', 'Created')";
-
-        if(!mysqli_query($con,$sql)) {
-            die('Error:' . mysqli_error($con));
-        }
-        else {
-            echo "<script>alert('This form has been submitted!');</script>";
-        }
-
-        mysqli_close($con);
-    }
-    ?>
+    
     <form method="post">
         <label for="title">Title:</label>
         <input type="text" id="title" name="title" required="required"><br>
