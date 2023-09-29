aaa32e5f1c25afca31a10b20c7718c31993ad584
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-30 00:56:29 +0800

    Update view_tmgt_acc.php

--- view_tmgt_acc.php
+++ view_tmgt_acc.php
@@ -1,6 +1,14 @@
 <a href="create_tmgt_acc.php">Add New Transport Manager Account</a><br>
 
-<?php include("conn.php");
+<?php 
+session_start();
+include("conn.php");
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit();
+}
+
 $result=mysqli_query($con,"SELECT* FROM transport_management");
 if (!$result) {
     die("Query failed: " . mysqli_error($con));
