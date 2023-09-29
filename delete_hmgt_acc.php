ecf8492190ac5bc207b85bd9572f404cd39b847d
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 21:30:58 +0800

    Update delete_hmgt_acc.php

--- delete_hmgt_acc.php
+++ delete_hmgt_acc.php
@@ -1,12 +1,19 @@
 <?php
-
 include("conn.php");
 
-$id=intval($_GET['hotel_manager_id']);
+$hotel_manager_id = $_GET['hotel_manager_id'];
 
-$result=mysqli_query($con,"DELETE FROM hotel_management WHERE hotel_manager_id=$id");
+// Use single quotes for string values in SQL queries
+$result = mysqli_query($con, "DELETE FROM hotel_management WHERE hotel_manager_id='$hotel_manager_id'");
+
+if ($result) {
+    // Optionally display a success message here if needed
+    // echo "Hotel Manager account deleted successfully.";
+} else {
+    echo "Error deleting hotel manager account: " . mysqli_error($con);
+}
 
 mysqli_close($con);
 header('Location: view_hmgt_acc.php');
-
 ?>
+
