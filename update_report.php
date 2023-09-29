21b41587e705b08a06405f77b579ec05deee820d
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 11:37:15 +0800

    Update update_report.php

--- update_report.php
+++ update_report.php
@@ -6,62 +6,79 @@
 <body>
     <h2>Report Update</h2>
     <?php
-        include("conn.php");
-        $report_id=intval($_GET['report_id']); 
-        $result=mysqli_query($con,"SELECT* FROM report WHERE id=$report_id");
-        while($row=mysqli_fetch_array($result))
-        {
-        $sql="UPDATE report SET
-        report_status = '$_POST[status]'
-        WHERE report_id = $_POST[report_id];";
-
-        if (mysqli_query($con,$sql)) {
-            mysqli_close($con);
-        }
-
+	include("conn.php");
+        if (isset($_GET['report_id'])) {
+            $report_id = intval($_GET['report_id']); 
+            $result = mysqli_query($con, "SELECT * FROM report WHERE report_id = $report_id");
+            
+            while ($row = mysqli_fetch_array($result)) {
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    // Handle form submission and update database here
+                    $report_id = $row['report_id']; // Get the report_id from the row
+                    $status = $_POST['status'];
+                    
+                    // Use prepared statements to prevent SQL injection
+                    $update_sql = "UPDATE ticket SET
+                        report_status = ?
+                        WHERE report_id = ?";
+                    
+                    $update_stmt = mysqli_prepare($con, $update_sql);
+                    mysqli_stmt_bind_param($update_stmt, "si", $status, $report_id);
+                    
+                    if (mysqli_stmt_execute($update_stmt)) {
+                        echo "<script>alert('Report information has been updated!');</script>";
+                    } else {
+                        echo "Error updating report information: " . mysqli_error($con);
+                    }
+                }
+                
+                // Display report information and form
     ?>
-        <p>
-        <label> Report ID: </label>
-		<input name='report_id' readonly='readonly' value="<?php echo $row['report_id']?>">
-		</p>
-		
-		<p>
-        <label> Support ID: </label>
-		<input name='sid' readonly='readonly' value="<?php echo $row['support_id']?>">
-		</p>
-		
-		<p>
-        <label> Report Title: </label>
-		<input name='title' readonly='readonly' value="<?php echo $row['report_title']?>">
-		</p>
+	
+                <p>
+                    <label> Report ID: </label>
+                    <input name='report_id' readonly='readonly' value="<?php echo $row['report_id']?>">
+                </p>
+                
+                <p>
+                    <label> Support ID: </label>
+                    <input name='sid' readonly='readonly' value="<?php echo $row['support_id']?>">
+                </p>
+                
+                <p>
+                    <label> Report Title: </label>
+                    <input name='title' readonly='readonly' value="<?php echo $row['report_title']?>">
+                </p>
+                
+                <p>
+                    <label> Report Priority: </label>
+                    <input name='priority' readonly='readonly' value="<?php echo $row['priority']?>">
+                </p>
 
-        <p>
-        <label> Report Priority: </label>
-		<input name='priority' readonly='readonly' value="<?php echo $row['priority']?>">
-		</p>
+                <p>
+                    <label> Report Description: </label>
+                    <input name='description' readonly='readonly' value="<?php echo $row['report_description']?>">
+                </p>
 
-        <p>
-        <label> Report Description: </label>
-		<input name='description' readonly='readonly' value="<?php echo $row['report_description']?>">
-		</p>
-
-
-        <p>
-        <form method="post">
-            <label for="status">Choose the Report Status:</label>
-            <select name="status" id="status" required="required">
-                <option value="">Please select the report status</option>
-                <option value="Created">Created</option>
-                <option value="Payment and Billing Support">Pending</option>
-                <option value="Feedback and Complaint Resolution">Resolved</option>
-                <option value="Closed">Closed</option>
-            </select><br>
-
-        <button type="submit" value="Submit">Update Report</button>
-        
-    </form><br>
+                <p>
+	        <form method="post">
+	            <label for="status">Choose the Report Status:</label>
+	            <select name="status" id="status" required="required">
+	                <option value="">Please select the report status</option>
+	                <option value="Created">Created</option>
+	                <option value="Payment and Billing Support">Pending</option>
+	                <option value="Feedback and Complaint Resolution">Resolved</option>
+	                <option value="Closed">Closed</option>
+	            </select><br>
+	
+	        <button type="submit" value="Submit">Update Report</button>
+                </form><br>
     <?php
-    }mysqli_close($con);
+            }
+            mysqli_close($con);
+        } else {
+            echo "Report ID not provided.";
+        }
     ?>
 </body>
 </html>
