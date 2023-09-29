2cb07899009ff5af4dc44426ccf8be550d6946e9
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 10:35:17 +0800

    Update homepage_support.php

--- homepage_support.php
+++ homepage_support.php
@@ -31,18 +31,38 @@ body {
     background-color: #C7949C;
     color: white;
 }
-
 </style>
 
 <body>
-        <nav>
-            <ul class="navibar">
-                <li><a href="support_report.php">REPORT</a></li>
-                <li><a href="view_own_support_report.php?support_id=<?php echo $row['support_id']; ?>">VIEW OWN REPORT</a></li>
-                <li><a href="view_request_form.php">VIEW REQUEST FORM</a></li>
-                <li><a href="logout.php" class="right">LOGOUT</a></li>
-                <li><a href="login.php" class="right">LOGIN</a></li>
-            </ul>
-        </nav>
+<nav>
+    <ul class="navibar">
+        <li><a href="support_report.php">REPORT</a></li>
+
+        <?php
+        // Include the database connection
+        include("conn.php");
+
+        // Query to fetch support_id values from your_table_name
+        $result = mysqli_query($con, "SELECT support_id FROM report");
+
+        // Check if the query was successful
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                // Generate dynamic links for each support_id
+                echo '<li><a href="view_own_support_report.php?support_id=' . $row['support_id'] . '">VIEW OWN REPORT</a></li>';
+            }
+
+            // Close the database connection
+            mysqli_close($con);
+        } else {
+            echo '<li>Error fetching data from the database.</li>';
+        }
+        ?>
+
+        <li><a href="view_request_form.php">VIEW REQUEST FORM</a></li>
+        <li><a href="logout.php" class="right">LOGOUT</a></li>
+        <li><a href="login.php" class="right">LOGIN</a></li>
+    </ul>
+</nav>
 </body>
 </html>
