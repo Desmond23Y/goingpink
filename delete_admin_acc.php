b1549caabdaeeef20325176d7b44b2b653a36323
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 17:45:06 +0800

    Update delete_admin_acc.php

--- delete_admin_acc.php
+++ delete_admin_acc.php
@@ -4,7 +4,7 @@ include("conn.php");
 
 $admin_id=$_GET['admin_id'];
 
-$result=mysqli_query($con,"DELETE * FROM admin WHERE admin_id=$admin_id");
+$result=mysqli_query($con,"DELETE FROM admin WHERE admin_id=$admin_id");
 
 mysqli_close($con);
 header('Location: view_admin_acc.php');
