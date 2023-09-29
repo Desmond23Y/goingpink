6e2b0fbc8aa52a1abd5ff406b6037a3915e213ac
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 20:34:12 +0800

    Update modify_admin_acc.php

--- modify_admin_acc.php
+++ modify_admin_acc.php
@@ -2,34 +2,38 @@
 // Include the database connection
 include("conn.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['name']) && isset($_POST['password'])) {
-    if (strlen($_POST['name']) < 5 || strlen($_POST['name']) > 50) {
-        echo "Length of username must be between 5 and 50 characters.";
-    } elseif (strlen($_POST['password']) < 5 || strlen($_POST['password']) > 50) {
-        echo "Password length must be between 5 and 50 characters. Please try again.";
-    } elseif (isset($_GET['admin_id'])) {
-        $admin_id = $_GET['admin_id']; 
-        $result = mysqli_query($con, "SELECT * FROM admin WHERE admin_id = '$admin_id'");
+if (isset($_GET['admin_id'])) {
+    $admin_id = $_GET['admin_id']; 
+    $result = mysqli_query($con, "SELECT * FROM admin WHERE admin_id = '$admin_id'");
 
-        while ($row = mysqli_fetch_array($result)) {
-            // Handle form submission and update database here
-            $admin_id = $row['admin_id']; // Get the admin_id from the row
+    while ($row = mysqli_fetch_array($result)) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $username = $_POST['name'];
             $password = $_POST['password'];
 
-            // Use prepared statements to prevent SQL injection
-            $update_sql = "UPDATE admin SET
-                username = ?,
-                password = ?
-                WHERE admin_id = ?";
-
-            $update_stmt = mysqli_prepare($con, $update_sql);
-            mysqli_stmt_bind_param($update_stmt, "sss", $username, $password, $admin_id);
-
-            if (mysqli_stmt_execute($update_stmt)) {
-                echo "<script>alert('This admin account has been modified!');</script>";
+            // Data validation
+            if (strlen($username) < 5 || strlen($username) > 50) {
+                echo "Length of username must be between 5 and 50 characters.";
+            } elseif (strlen($password) < 5 || strlen($password) > 50) {
+                echo "Password length must be between 5 and 50 characters. Please try again.";
             } else {
-                echo "Error updating admin account: " . mysqli_error($con);
+                // Handle form submission and update database here
+                $admin_id = $row['admin_id']; // Get the hotel_manager_id from the row
+
+                // Use prepared statements to prevent SQL injection
+                $update_sql = "UPDATE admin SET
+                    username = ?,
+                    password = ?
+                    WHERE admin_id = ?";
+
+                $update_stmt = mysqli_prepare($con, $update_sql);
+                mysqli_stmt_bind_param($update_stmt, "sss", $username, $password, $admin_id);
+
+                if (mysqli_stmt_execute($update_stmt)) {
+                    echo "<script>alert('This admin account has been modified!');</script>";
+                } else {
+                    echo "Error updating admin account: " . mysqli_error($con);
+                }
             }
         }
         ?>
