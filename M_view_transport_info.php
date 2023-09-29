c7f2fa77004dae4e8132e13ead02f1b9b93be610
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-30 00:34:31 +0800

    123

--- M_view_transport_info.php
+++ M_view_transport_info.php
@@ -19,10 +19,10 @@ if (!$result) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel Information</title>
     <link rel="stylesheet" href="M_navibar.css">
-     <nav>
+    <nav>
         <ul class="navibar">
-            <li><a href="M_hotel_homepage.php">HOME</a></li>
-            <li><a href="M_view_transport_info.php">TRANSPORT INFO</a></li>
+            <li><a href="M_transport_homepage.php">HOME</a></li>
+            <li><a href="M_view_transport_info.php">TRANSPORTS INFO</a></li>
             <li><a href="M_view_transport_booking.php">TRANSPORT BOOKING</a></li>
             <li><a href="logout.php" class="right">LOGOUT</a></li>
         </ul>
