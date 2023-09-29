0db94ef3862c9eaa7bce6802ad8edb2311f5fe1a
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-30 00:32:09 +0800

    Update and rename admin_update_transportation_booking.php to admin_cancel_transportation_booking.php

---
+++ admin_cancel_transportation_booking.php
@@ -0,0 +1,18 @@
+<?php
+include("conn.php");
+
+$transport_booking_id = $_GET['transport_booking_id'];
+
+// Use single quotes for string values in SQL queries
+$result = mysqli_query($con, "DELETE FROM transportation_booking WHERE transport_booking_id='$transport_booking_id'");
+
+if ($result) {
+    // Optionally display a success message here if needed
+    // echo "Hotel Manager account deleted successfully.";
+} else {
+    echo "Error deleting transport booking: " . mysqli_error($con);
+}
+
+mysqli_close($con);
+header('Location: admin_view_transport_booking.php');
+?>
