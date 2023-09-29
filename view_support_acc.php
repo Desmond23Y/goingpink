b3a7f8a45f0f2ece51292890371f93fd5a041320
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 20:04:17 +0800

    Update view_support_acc.php

--- view_support_acc.php
+++ view_support_acc.php
@@ -5,6 +5,8 @@ $result=mysqli_query($con,"SELECT* FROM support");
     <tr bgcolor="#FFB6C1">
         <td>Support ID</td>
         <td>Username</td>
+        <td>Edit</td>
+        <td>Delete</td>
     </tr>
 
 <?php
@@ -13,6 +15,8 @@ $result=mysqli_query($con,"SELECT* FROM support");
         echo'<tr>';
         echo'<td>'.$row["support_id"].'</td>';
         echo'<td>'.$row["username"].'</td>';
+        echo'<td><a href="modify_support_acc.php?support_id='.$row["support_id"].'">Edit</a></td>';
+        echo'<td><a onclick="return confirm(\'Delete '.$row["username"].' details?\')" href="delete_support_acc.php?support_id='.$row["support_id"].'">Delete</a></td>';
         echo'</tr>';
     }
     mysqli_close($con);
