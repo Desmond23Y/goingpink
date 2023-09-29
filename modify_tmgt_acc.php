df4158c7e52a2dda94c9382223b5be5e958cc8e9
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 20:17:20 +0800

    Update modify_tmgt_acc.php

--- modify_tmgt_acc.php
+++ modify_tmgt_acc.php
@@ -1,44 +1,56 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include("conn.php");
-    if (strlen($_POST['name']) > 5 && strlen($_POST['name']) < 50) {
-        echo "Length of username must be between 5 and 50 characters.";
-    } elseif (strlen($_POST['password']) > 5 && strlen($_POST['password']) < 50) {
-        echo "Password length must be between 5 and 50 characters. Please try again.";
-    } else
-        $transport_management_id=intval($_GET['transport_management_id']); 
-        $result=mysqli_query($con,"SELECT* FROM transport_management WHERE id=$transport_management_id");
-        while($row=mysqli_fetch_array($result))
-        {
-        $sql="UPDATE transport_management SET
-        username = '$_POST[name]',
-        password = '$_POST[password]',
+// Include the database connection
+include("conn.php");
 
-        WHERE transport_management_id = $transport_management_id;";
+if (isset($_GET['transport_manager_id'])) {
+    $transport_manager_id = $_GET['transport_manager_id']; 
+    $result = mysqli_query($con, "SELECT * FROM transport_management WHERE transport_manager_id = '$transport_manager_id'");
 
-    if(!mysqli_query($con,$sql)) {
-        die('Error:' . mysqli_error($con));
+    while ($row = mysqli_fetch_array($result)) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Handle form submission and update database here
+            $transport_manager_id = $row['transport_manager_id']; // Get the transport_manager_id from the row
+            $username = $_POST['name'];
+            $password = $_POST['password'];
+
+            // Use prepared statements to prevent SQL injection
+            $update_sql = "UPDATE transport_management SET
+                username = ?,
+                password = ?
+                WHERE transport_manager_id = ?";
+
+            $update_stmt = mysqli_prepare($con, $update_sql);
+            mysqli_stmt_bind_param($update_stmt, "sss", $username, $password, $transport_manager_id);
+
+            if (mysqli_stmt_execute($update_stmt)) {
+                echo "<script>alert('This transport manager account has been modified!');</script>";
+            } else {
+                echo "Error updating transport manager account: " . mysqli_error($con);
+            }
+        }
+        ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Modify Transport Manager Account</title>
+        </head>
+        <body>
+            <h2>Transport Manager Account Modification</h2>
+            <form method="post" action="modify_tmgt_acc.php?transport_manager_id=<?php echo $transport_manager_id; ?>">
+                <label for="name">Username:</label>
+                <input type="text" id="name" name="name" required="required" value="<?php echo $row['username']; ?>"><br>
+        
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" maxlength="50" required value="<?php echo $row['password']; ?>"><br><br>
+        
+                <button type="submit">Edit Transport Manager Account</button>
+            </form>
+        <?php
+        }
+        mysqli_close($con);
+    } else {
+        echo "Transport Manager ID not provided.";
     }
-    else {
-        echo "<script>alert('This transport management account has been modified!');</script>";
-    }
-
-    mysqli_close($con);
-}
-}
 ?>
-
-<!DOCTYPE html>
-<body>
-    <form method="post">
-        <label for="name">Username:</label>
-        <input type="text" id="name" name="name" required="required" value="<?php echo $row['username']?>"><br>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" maxlength="50" required value="<?php echo $row['password']?>"><br><br>
-
-        <button type="submit">Edit Management Account</button>
-    </form>
 </body>
 </html>
-
