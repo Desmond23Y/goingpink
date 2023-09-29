49dfb4da2b67c92b6a696f423d9e7369e59a237f
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 21:35:58 +0800

    Update delete_tmgt_acc.php

--- delete_tmgt_acc.php
+++ delete_tmgt_acc.php
@@ -1,12 +1,18 @@
 <?php
-
 include("conn.php");
 
-$id=intval($_GET['transport_manager_id']);
+$transport_manager_id = $_GET['transport_manager_id'];
 
-$result=mysqli_query($con,"DELETE FROM transport_management WHERE transport_manager_id=$id");
+// Use single quotes for string values in SQL queries
+$result = mysqli_query($con, "DELETE FROM transport_management WHERE transport_manager_id='$transport_manager_id'");
+
+if ($result) {
+    // Optionally display a success message here if needed
+    // echo "Transport Manager account deleted successfully.";
+} else {
+    echo "Error deleting transport manager account: " . mysqli_error($con);
+}
 
 mysqli_close($con);
 header('Location: view_tmgt_acc.php');
-
 ?>
