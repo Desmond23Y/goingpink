dcb1ed998f24c9d7b2e169ac8fe2d7bef6e1321b
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 15:58:30 +0800

    Update create_admin_acc.php

--- create_admin_acc.php
+++ create_admin_acc.php
@@ -1,18 +1,18 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include("conn.php");
-    if (strlen($_POST['name']) > 5 && strlen($_POST['name']) < 50) {
+    if (strlen($_POST['name']) < 5 || strlen($_POST['name']) > 50) {
         echo "Length of username must be between 5 and 50 characters.";
-    } elseif (strlen($_POST['password']) > 5 && strlen($_POST['password']) < 50) {
+    } elseif (strlen($_POST['password']) < 5 || strlen($_POST['password']) > 50) {
         echo "Password length must be between 5 and 50 characters. Please try again.";
-    } else
+    } else {
         $sql = "INSERT INTO admin (username, password) VALUES ('$_POST[name]', '$_POST[password]')";
-
-    if(!mysqli_query($con,$sql)) {
-        die('Error:' . mysqli_error($con));
-    }
-    else {
-        echo "<script>alert('This admin account has been created!');</script>";
+        
+        if(!mysqli_query($con, $sql)) {
+            die('Error: ' . mysqli_error($con));
+        } else {
+            echo "<script>alert('This admin account has been created!');</script>";
+        }
     }
 
     mysqli_close($con);
@@ -32,4 +32,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </form>
 </body>
 </html>
-
