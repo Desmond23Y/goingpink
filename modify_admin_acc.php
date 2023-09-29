6ce848b418ebe670042ffa02d3f4dc11621e42fa
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 16:21:37 +0800

    Update modify_admin_acc.php

--- modify_admin_acc.php
+++ modify_admin_acc.php
@@ -1,40 +1,45 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include("conn.php");
-    
-    if (strlen($_POST['name']) < 5 || strlen($_POST['name']) > 50) {
-        echo "Length of username must be between 5 and 50 characters.";
-    } elseif (strlen($_POST['password']) < 5 || strlen($_POST['password']) > 50) {
-        echo "Password length must be between 5 and 50 characters. Please try again.";
-    } else {
-        $admin_id = intval($_GET['admin_id']);
-        $username = $_POST['name'];
-        $password = $_POST['password'];
-        
-        // Use prepared statements to prevent SQL injection
-        $update_sql = "UPDATE admin SET username = ?, password = ? WHERE admin_id = ?";
-        $update_stmt = mysqli_prepare($con, $update_sql);
-        mysqli_stmt_bind_param($update_stmt, "ssi", $username, $password, $admin_id);
-        
-        if (mysqli_stmt_execute($update_stmt)) {
-            echo "<script>alert('This admin account has been modified!');</script>";
-        } else {
-            echo "Error updating admin account: " . mysqli_error($con);
-        }
+// Include the database connection
+include("conn.php");
 
-        mysqli_close($con);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get admin_id from the URL parameter
+    $admin_id = $_GET['admin_id'];
+
+    // Validate and sanitize the admin_id (you should implement proper validation)
+    // For now, we assume it's a valid admin_id
+
+    $username = $_POST['name'];
+    $password = $_POST['password'];
+
+    // Use prepared statements to prevent SQL injection
+    $update_sql = "UPDATE admin SET username = ?, password = ? WHERE admin_id = ?";
+    $update_stmt = mysqli_prepare($con, $update_sql);
+    mysqli_stmt_bind_param($update_stmt, "ssi", $username, $password, $admin_id);
+
+    if (mysqli_stmt_execute($update_stmt)) {
+        echo "<script>alert('This admin account has been modified!');</script>";
+    } else {
+        echo "Error updating admin account: " . mysqli_error($con);
     }
+
+    mysqli_close($con);
 }
 
-// Fetch the admin data before the HTML form
-$admin_id = intval($_GET['admin_id']);
-$result = mysqli_query($con, "SELECT * FROM admin WHERE admin_id = $admin_id");
+// Fetch the admin data based on admin_id
+$admin_id = $_GET['admin_id'];
+$result = mysqli_query($con, "SELECT * FROM admin WHERE admin_id = '$admin_id'");
 $row = mysqli_fetch_array($result);
 ?>
 
 <!DOCTYPE html>
+<html>
+<head>
+    <title>Modify Admin Account</title>
+</head>
 <body>
-    <form method="post">
+    <h2>Admin Account Modification</h2>
+    <form method="post" action="modify_admin_acc.php?admin_id=<?php echo $admin_id; ?>">
         <label for="name">Username:</label>
         <input type="text" id="name" name="name" required="required" value="<?php echo $row['username']; ?>"><br>
 
@@ -45,3 +50,4 @@ $row = mysqli_fetch_array($result);
     </form>
 </body>
 </html>
+
