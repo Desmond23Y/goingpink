b47d0fcda8439daece2ebbe3a68ce9032235677e
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 22:13:56 +0800

    Update homepage_admin.php

--- homepage_admin.php
+++ homepage_admin.php
@@ -1,1 +1,42 @@
-
+<?php
+session_start()
+?>
+<!DOCTYPE html>
+<html>
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    .navibar {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #F9B9C3;
+    }
+    .navibar a {
+        float: left;
+        display: block;
+        color: #333;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+    .navibar a.right {
+        float: right;
+    }
+    .navibar a:hover {
+        background-color: #C7949C;
+        color: white;
+    }
+</style>
+<body>
+    <nav>
+        <ul class="navibar">
+            <li><a href="logout.php" class="right">LOGOUT</a></li>
+            <li><a href="login.php" class="right">LOGIN</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
