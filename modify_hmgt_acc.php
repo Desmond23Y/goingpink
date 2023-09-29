6dbce48894ee4191485cc7615343b54421dd49fa
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 20:41:24 +0800

    Update modify_hmgt_acc.php

--- modify_hmgt_acc.php
+++ modify_hmgt_acc.php
@@ -8,24 +8,32 @@ if (isset($_GET['hotel_manager_id'])) {
 
     while ($row = mysqli_fetch_array($result)) {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Handle form submission and update database here
-            $hotel_manager_id = $row['hotel_manager_id']; // Get the hotel_manager_id from the row
             $username = $_POST['name'];
             $password = $_POST['password'];
 
-            // Use prepared statements to prevent SQL injection
-            $update_sql = "UPDATE hotel_management SET
-                username = ?,
-                password = ?
-                WHERE hotel_manager_id = ?";
-
-            $update_stmt = mysqli_prepare($con, $update_sql);
-            mysqli_stmt_bind_param($update_stmt, "sss", $username, $password, $hotel_manager_id);
-
-            if (mysqli_stmt_execute($update_stmt)) {
-                echo "<script>alert('This hotel manager account has been modified!');</script>";
+            // Data validation
+            if (strlen($username) < 5 || strlen($username) > 50) {
+                echo "Length of username must be between 5 and 50 characters.";
+            } elseif (strlen($password) < 5 || strlen($password) > 50) {
+                echo "Password length must be between 5 and 50 characters. Please try again.";
             } else {
-                echo "Error updating hotel manager account: " . mysqli_error($con);
+                // Handle form submission and update database here
+                $hotel_manager_id = $row['hotel_manager_id']; // Get the hotel_manager_id from the row
+
+                // Use prepared statements to prevent SQL injection
+                $update_sql = "UPDATE hotel_management SET
+                    username = ?,
+                    password = ?
+                    WHERE hotel_manager_id = ?";
+
+                $update_stmt = mysqli_prepare($con, $update_sql);
+                mysqli_stmt_bind_param($update_stmt, "sss", $username, $password, $hotel_manager_id);
+
+                if (mysqli_stmt_execute($update_stmt)) {
+                    echo "<script>alert('This hotel manager account has been modified!');</script>";
+                } else {
+                    echo "Error updating hotel manager account: " . mysqli_error($con);
+                }
             }
         }
         ?>
