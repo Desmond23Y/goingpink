65d422c8908774831298a2771092abc45985cdce
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 10:12:56 +0800

    Update view_own_support_report.php

--- view_own_support_report.php
+++ view_own_support_report.php
@@ -5,8 +5,8 @@ if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
     exit();
 }
-if (isset($_GET['id'])) {
-    $support_id = $_GET['id'];
+if (isset($_SESSION['id'])) {
+    $support_id = $_SESSION['id'];
     $result=mysqli_query($con,"SELECT* FROM report WHERE id=$support_id");
 }
 ?>
