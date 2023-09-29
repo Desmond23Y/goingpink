259982cf87c9a4541fa5e57c43b2c03862931670
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 21:39:18 +0800

    Update delete_user_acc.php

--- delete_user_acc.php
+++ delete_user_acc.php
@@ -1,12 +1,18 @@
 <?php
-
 include("conn.php");
 
-$id=intval($_GET['user_id']);
+$user_id = $_GET['user_id'];
 
-$result=mysqli_query($con,"DELETE FROM user WHERE user_id=$id");
+// Use single quotes for string values in SQL queries
+$result = mysqli_query($con, "DELETE FROM user WHERE user_id='$user_id'");
+
+if ($result) {
+    // Optionally display a success message here if needed
+    // echo "User account deleted successfully.";
+} else {
+    echo "Error deleting user account: " . mysqli_error($con);
+}
 
 mysqli_close($con);
 header('Location: view_user_acc.php');
-
 ?>
