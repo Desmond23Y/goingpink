54851d20a14bded7e9921f2b62bed79bc5a62ad3
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 19:48:25 +0800

    Update delete_admin_acc.php

--- delete_admin_acc.php
+++ delete_admin_acc.php
@@ -1,19 +1,18 @@
 <?php
-
 include("conn.php");
 
-$admin_id=$_GET['admin_id'];
+$admin_id = $_GET['admin_id'];
 echo "Admin ID: " . $admin_id;
 
-$result=mysqli_query($con,"DELETE FROM admin WHERE admin_id=$admin_id");
+// Use single quotes for string values in SQL queries
+$result = mysqli_query($con, "DELETE FROM admin WHERE admin_id='$admin_id'");
 
 if ($result) {
-    echo "Admin account deleted successfully."; // Add this line for debugging
+    echo "Admin account deleted successfully.";
 } else {
     echo "Error deleting admin account: " . mysqli_error($con);
 }
 
 mysqli_close($con);
 header('Location: view_admin_acc.php');
-
 ?>
