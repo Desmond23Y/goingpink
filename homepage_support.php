6fd059361af5d6bb5d0fc11e5e1dd44fae1746c1
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 22:10:02 +0800

    Update homepage_support.php

--- homepage_support.php
+++ homepage_support.php
@@ -41,7 +41,6 @@ session_start()
         <ul class="navibar">
             <li><a href="support_report.php">REPORT</a></li>
             <li><a href="view_own_support_report.php?user_id=<?php echo $_SESSION['user_id']; ?>">VIEW OWN REPORT</a></li>
-            <li><a href="view_request_form.php">VIEW REQUEST FORM</a></li>
             <li><a href="view_ticket.php">VIEW TICKET</a></li>
             <li><a href="logout.php" class="right">LOGOUT</a></li>
             <li><a href="login.php" class="right">LOGIN</a></li>
