c3785054c58687ea10e03c91b7a8a1ebb85c8750
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-09-29 20:05:22 +0800

    Update view_hmgt_acc.php

--- view_hmgt_acc.php
+++ view_hmgt_acc.php
@@ -8,6 +8,8 @@ if (!$result) {
     <tr bgcolor="#FFB6C1">
         <td>Hotel Manager ID</td>
         <td>Username</td>
+        <td>Edit</td>
+        <td>Delete</td>
     </tr>
 
 <?php
@@ -16,6 +18,8 @@ if (!$result) {
         echo'<tr>';
         echo'<td>'.$row["hotel_manager_id"].'</td>';
         echo'<td>'.$row["username"].'</td>';
+        echo'<td><a href="modify_hmgt_acc.php?hotel_manager_id='.$row["hotel_manager_id"].'">Edit</a></td>';
+        echo'<td><a onclick="return confirm(\'Delete '.$row["username"].' details?\')" href="delete_hmgt_acc.php?hotel_manager_id='.$row["hotel_manager_id"].'">Delete</a></td>';
         echo'</tr>';
     }
     mysqli_close($con);
