96a40f548e684b9a722bac07d8f7c436aef3a052
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-09-30 21:58:55 +0800

    Update index.php
    updated carousel indentation

--- index.php
+++ index.php
@@ -31,42 +31,43 @@ include('navi_bar.php');
 
 <body>
     <!--Carousel-->
-    <div id="carouselExampleIndicators" class="carousel slide">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="/picture/city.png" width =1440px height= "597px" class="d-block w-100" alt="purple city">
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
 
-            <div class="carousel-title-caption">
-                <p >Creating Memories,<br> One <span style="color:#F1ACA4">Pink</span> Trip at a Time.</p>
-            </div>
-            <div class="carousel-caption">
-                <p>“The only Travel System Service Management <br> you ever need.”</p>
-            </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/picture/city.png" width="1440px" height="597px" class="d-block w-100" alt="purple city">
 
-
-
-          </div>
-          <div class="carousel-item">
-            <img src="/picture/download (1).png" width =1440px height= "597px" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="/picture/download.png" width =1440px height= "597px" class="d-block w-100" alt="...">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+      <div class="carousel-title-caption">
+        <p>Creating Memories,<br> One <span style="color:#F1ACA4">Pink</span> Trip at a Time.</p>
       </div>
+      <div class="carousel-caption">
+        <p>“The only Travel System Service Management <br> you ever need.”</p>
+      </div>
+   </div>
+
+    <div class="carousel-item">
+      <img src="/picture/download (1).png" width="1440px" height="597px" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/picture/download.png" width="1440px" height="597px" class="d-block w-100" alt="...">
+    </div>
+  </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
 
     <!--Reasons to Use-->
 
