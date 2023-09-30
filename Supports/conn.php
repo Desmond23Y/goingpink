f987e1811dfb221ebce70d9e51ded6532bbbb0f0
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-09-30 23:57:46 +0800

    123

---
+++ Supports/conn.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("localhost","id21215862_goingpink","@Goingpink123","id21215862_goingpink");
+
+if ($con->connect_error) {
+    die("Could not connect to the server!: " . $con->connect_error);
+}
+?>
