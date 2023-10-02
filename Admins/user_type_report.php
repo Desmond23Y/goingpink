5b8999711c9142483f644b8c5a3f332dfcaedbc6
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-10-03 00:41:26 +0800

    Update user_type_report.php

--- Admins/user_type_report.php
+++ Admins/user_type_report.php
@@ -9,9 +9,6 @@
     // Database connection
     include('conn.php');
 
-    if(!mysqli_query($con,$sql)) {
-        die('Error:' . mysqli_error($con));
-     }
     // Query data from Table 1
     $queryTable1 = "SELECT user_id, COUNT(*) as count FROM user";
     $resultTable1 = mysqli_query($conn, $queryTable1);
