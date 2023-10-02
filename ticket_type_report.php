dc22a96a7d1b4b39b18bdcee786af383cc920e95
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-10-03 02:02:22 +0800

    Create ticket_type_report.php

---
+++ ticket_type_report.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Support Type Line Chart</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+    <?php
+    // Database connection
+    include('conn.php');
+
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Query data for support types
+    $querySupportTypes = "SELECT support_type, COUNT(*) as count FROM ticket GROUP BY support_type";
+    $resultSupportTypes = mysqli_query($con, $querySupportTypes);
+
+    // Initialize arrays to store data
+    $supportTypeLabels = [];
+    $supportTypeCounts = [];
+
+    while ($row = mysqli_fetch_assoc($resultSupportTypes)) {
+        $supportTypeLabels[] = $row['support_type'];
+        $supportTypeCounts[] = $row['count'];
+    }
+
+    // Close the database connection
+    mysqli_close($con);
+    ?>
+
+    <h2>Support Type Counts</h2>
+    <canvas id="lineChart" width="400" height="200"></canvas>
+
+    <script>
+        var ctx = document.getElementById('lineChart').getContext('2d');
+        var chartData = {
+            labels: <?php echo json_encode($supportTypeLabels); ?>,
+            datasets: [{
+                label: 'Support Type Counts',
+                data: <?php echo json_encode($supportTypeCounts); ?>,
+                fill: true,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        };
+
+        var lineChart = new Chart(ctx, {
+            type: 'line',
+            data: chartData,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
+</body>
+</html>
