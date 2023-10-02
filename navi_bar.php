751b106fba61f5763fdb8fc540cb26e2bd9aa381
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-10-02 14:54:30 +0800

    123

--- navi_bar.php
+++ navi_bar.php
@@ -38,17 +38,17 @@ body {
         <nav>
             <ul class="navibar">
                 <li><a href="index.php">HOME</a></li>
-                <li><a href="Users/viewhotel.php">HOTEL</a></li>
-                <li><a href="Users/viewtransport.php">TRANSPORT</a></li>
+                <li><a href="../Users/viewhotel.php">HOTEL</a></li>
+                <li><a href="../Users/viewtransport.php">TRANSPORT</a></li>
                 <li><a href="api_exRates.php">EXCHANGE RATES</a></li>
                 <li><a href="request_form.php">SUPPORT</a></li>
-                <li><a href="Users/view_rating.php">VIEW RATING</a></li>
-                <li><a href="Users/rating.php">LEAVE A RATING</a></li>
+                <li><a href="../Users/view_rating.php">VIEW RATING</a></li>
+                <li><a href="../Users/rating.php">LEAVE A RATING</a></li>
                 <li><a href="logout.php" class="right">LOGOUT</a></li>
                 <li><a href="register.php" class="right">REGISTER</a></li>
                 <li><a href="login.php" class="right">LOGIN</a></li>
-                <li><a href="Users/view_profile.php" class="right">PROFILE</a></li>
-                <li><a href="Users/user_view_ticket.php" class="right">VIEW TICKETS</a></li>
+                <li><a href="../Users/view_profile.php" class="right">PROFILE</a></li>
+                <li><a href="../Users/user_view_ticket.php" class="right">VIEW TICKETS</a></li>
                 <li><a href="../Users/faq.php" class="right">FAQ</a></li>
             </ul>
         </nav>
