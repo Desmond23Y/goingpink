a1e8d867e4a26911db5d0f06f9c6e27aa2060d3f
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-10-02 16:27:06 +0800

    1

--- Users/view_hotel_payment.php
+++ Users/view_hotel_payment.php
@@ -2,20 +2,20 @@
 session_start();
 include('conn.php');
 
-$user_id = $_SESSION['user_id'];
-$hotel_id = $_SESSION['selected_hotel_id'];
-
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
     exit();
 }
 
-if (isset($_GET['hotel_id' && 'user_id'])) {
+$user_id = $_SESSION['user_id'];
+$hotel_id = $_SESSION['selected_hotel_id'];
+
+if (isset($_GET['hotel_id']) && isset($_GET['user_id'])) {
     $hotel_id = $_GET['hotel_id'];
     $result = mysqli_query($con, "SELECT * FROM hotel_booking WHERE hotel_id = '$hotel_id'");
     $result2 = mysqli_query($con, "SELECT * FROM user WHERE user_id = '$user_id'");
     $result3 = mysqli_query($con, "SELECT * FROM hotel_information WHERE hotel_id = '$hotel_id'");
-    if (!$result && $result2 && $result3) {
+    if (!$result || !$result2 || !$result3) {
         die('Query Error: ' . mysqli_error($con));
     }
 }
@@ -26,15 +26,15 @@ if (isset($_GET['hotel_id' && 'user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Booking Summary</title>
-    <link rel="stylesheet" href=".css">
+    <link rel="stylesheet" href="your_css_file.css">
 </head>
 
 <body>
 <h1>Fare Summary</h1>
     <div class="box">
         <?php
-        if ($result && $result2 && $result3-> num_rows > 0) {
-            while ($row = mysqli_fetch_assoc($result && $result2 && $result3)) {
+        if (isset($result) && isset($result2) && isset($result3) && mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
                 echo '<h3>' . $row["username"] . '</h3>';
                 echo '<h3> Phone number: ' . $row["phone_number"] . '</h3>';
                 echo '<h3> Email: ' . $row["email"] . '</h3>';
