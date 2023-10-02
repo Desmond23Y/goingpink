8e5f9c53dc13adc55cd44a4bcc1fc398a6044bd7
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-02 23:36:39 +0800

    Update homepage_admin.php

--- Admins/homepage_admin.php
+++ Admins/homepage_admin.php
@@ -11,17 +11,16 @@ session_start()
     <!-- Include Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <!--Import Butler & Futura Font-->
-    <style>
-    @import url('https://fonts.cdnfonts.com/css/butler');
-    @import url('https://fonts.cdnfonts.com/css/futura-pt');
-    </style>
 
    <!--Javascript-->
    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 
     <style>
+       <!--Import Butler & Futura Font-->
+        @import url('https://fonts.cdnfonts.com/css/butler');
+        @import url('https://fonts.cdnfonts.com/css/futura-pt');
+        
         body {
             margin: 0;
             padding: 0;
