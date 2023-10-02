e572942b68113dd94b4878fc5a97050168ed8051
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-10-03 02:21:52 +0800

    123

--- Users/hotel_payment.php
+++ Users/hotel_payment.php
@@ -8,8 +8,8 @@
     <form action="" method="POST">
         <label for="card_information">Card Information</label>
         <input type="text" name="card_information" pattern="[0-9]{16}" placeholdertle="1234 1234 1234 1234" required>
-        <input type="date" id="expiration_date" name="expiration_date" pattern="^(0[1-9]|1[0-2])\/\d{2}$" placeholder="MM/YY" required>
-        <input type="text" id="cvc" name="cvc" pattern="[0-9]{3}" placeholder="MM/YY" required><br><br>
+        <input type="text" id="expiration_date" name="expiration_date" pattern="^(0[1-9]|1[0-2])\/\d{2}$" placeholder="MM/YY" required>
+        <input type="text" id="cvc" name="cvc" pattern="[0-9]{3}" placeholder="CVC" required><br><br>
 
         <label for="cardholder name">Cardholder name</label>
         <input type="text" name="cardholder name" placeholder="Full Name on Card" required><br><br>
