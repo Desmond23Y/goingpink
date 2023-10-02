cc72f681df727560f7a4d372c68138c14db63b9b
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-03 00:20:50 +0800

    Update M_hotel_homepage.php

--- HotelManagers/M_hotel_homepage.php
+++ HotelManagers/M_hotel_homepage.php
@@ -8,6 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Hotel Management's Homepage</title>
     <link rel="stylesheet" href="M_navibar.css">
+     <!-- Include Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!--Import Butler & Futura Font-->
+    <style>
+    @import url('https://fonts.cdnfonts.com/css/butler');
+    @import url('https://fonts.cdnfonts.com/css/futura-pt');
+    </style>
     <nav>
         <ul class="navibar">
             <li><a href="M_hotel_homepage.php">HOME</a></li>
