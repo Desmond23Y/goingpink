b6e30cb064a8ee3d84a29754302f9c7e66dd3e22
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-10-03 02:23:31 +0800

    Create star_rating_report.php

---
+++ Admins/star_rating_report.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Star Rating Statistics</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+    <?php
+    // Database connection
+    include('conn.php');
+
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Query data from the "rating" table
+    $query = "SELECT rating_id, COUNT(*) as count FROM rating GROUP BY total_stars_rating";
+    $result = mysqli_query($conn, $query);
+
+    // Initialize arrays to store data
+    $starRatings = [];
+    $ratingCounts = [];
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $starRatings[] = $row['star_rating'];
+        $ratingCounts[] = $row['count'];
+    }
+
+    // Close the database connection
+    mysqli_close($conn);
+    ?>
+
+    <h2>Star Rating Statistics</h2>
+    <canvas id="horizontalBarChart" width="400" height="200"></canvas>
+
+    <script>
+        var ctx = document.getElementById('horizontalBarChart').getContext('2d');
+        var chartData = {
+            labels: <?php echo json_encode($starRatings); ?>,
+            datasets: [{
+                label: 'Star Rating Counts',
+                data: <?php echo json_encode($ratingCounts); ?>,
+                backgroundColor: 'rgba(75, 192, 192, 0.5)', // Bar color
+                borderColor: 'rgba(75, 192, 192, 1)', // Border color
+                borderWidth: 1
+            }]
+        };
+
+        var horizontalBarChart = new Chart(ctx, {
+            type: 'horizontalBar',
+            data: chartData,
+            options: {
+                scales: {
+                    x: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
+</body>
+</html>
