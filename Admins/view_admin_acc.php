784debe5c97938d62e48a3dace0d48d5be8b47d7
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-10-03 14:20:23 +0800

    Update view_admin_acc.php

--- Admins/view_admin_acc.php
+++ Admins/view_admin_acc.php
@@ -1,35 +1,52 @@
-<a href="create_admin_acc.php">Add New Admin Account</a><br>
-
-<?php 
+<?php
+// Start a session (must be the first thing in the script)
 session_start();
+
+// Include the database connection file
 include("conn.php");
 
+// Check if the user is authenticated
 if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
-    exit();
+    header('Location: login.php'); // Redirect to the login page if not authenticated
+    exit(); // Terminate script execution
 }
 
-$result=mysqli_query($con,"SELECT* FROM admin");
+// Execute a MySQL query to fetch admin records
+$result = mysqli_query($con, "SELECT * FROM admin");
+
 ?>
-<table width="90%">
-    <tr bgcolor="#FFB6C1">
-        <td>Admin ID</td>
-        <td>Username</td>
-        <td>Edit</td>
-        <td>Delete</td>
-    </tr>
 
-<?php
-    while($row=mysqli_fetch_array($result))
-    {
-        echo'<tr>';
-        echo'<td>'.$row["admin_id"].'</td>';
-        echo'<td>'.$row["username"].'</td>';
-        echo'<td><a href="modify_admin_acc.php?admin_id='.$row["admin_id"].'">Edit</a></td>';
-        echo'<td><a onclick="return confirm(\'Delete '.$row["username"].' details?\')" href="delete_admin_acc.php?admin_id='.$row["admin_id"].'">Delete</a></td>';
-        echo'</tr>';
+<!DOCTYPE html>
+<html>
+<head>
+    <title>View Admin Accounts</title>
+</head>
+<body>
+    <a href="create_admin_acc.php">Add New Admin Account</a><br>
 
-    }
+    <table width="90%">
+        <tr bgcolor="#FFB6C1">
+            <td>Admin ID</td>
+            <td>Username</td>
+            <td>Edit</td>
+            <td>Delete</td>
+        </tr>
+
+        <?php
+        while ($row = mysqli_fetch_array($result)) {
+            echo '<tr>';
+            echo '<td>' . $row["admin_id"] . '</td>';
+            echo '<td>' . $row["username"] . '</td>';
+            echo '<td><a href="modify_admin_acc.php?admin_id=' . $row["admin_id"] . '">Edit</a></td>';
+            echo '<td><a onclick="return confirm(\'Delete ' . $row["username"] . ' details?\')" href="delete_admin_acc.php?admin_id=' . $row["admin_id"] . '">Delete</a></td>';
+            echo '</tr>';
+        }
+        ?>
+    </table>
+
+    <?php
+    // Close the MySQL database connection
     mysqli_close($con);
-?>
-</table>
+    ?>
+</body>
+</html>
