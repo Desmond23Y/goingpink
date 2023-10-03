1a8547bb7397efaea231f9a0be4bb34515e7964d
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-03 10:11:53 +0800

    Update homepage_support.php
    updated head

--- Supports/homepage_support.php
+++ Supports/homepage_support.php
@@ -3,38 +3,25 @@ session_start()
 ?>
 <!DOCTYPE html>
 <html>
-<style>
-    body {
-        margin: 0;
-        padding: 0;
-    }
-
-    .navibar {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: #F9B9C3;
-    }
-
-    .navibar a {
-        float: left;
-        display: block;
-        color: #333;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-    }
-
-    .navibar a.right {
-        float: right;
-    }
-
-    .navibar a:hover {
-        background-color: #C7949C;
-        color: white;
-    }
-</style>
+     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GoingPink | Support</title>
+        <link rel="stylesheet" href="support.css">
+        <!-- Include Bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+        <!--Import Butler & Futura Font-->
+        <style>
+        @import url('https://fonts.cdnfonts.com/css/butler');
+        @import url('https://fonts.cdnfonts.com/css/futura-pt');
+        </style>
+    
+       <!--Javascript-->
+       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    
+    </head>
 
 <body>
     <nav>
