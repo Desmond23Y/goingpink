7c03a56e7342f37665dede75abbb673187265fc6
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-10-03 15:02:03 +0800

    Update request_form.php

--- Users/request_form.php
+++ Users/request_form.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
 include('conn.php');
-include('../navi_bar.php');
 
 // Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
-    header('Location:../login.php'); // Redirect to the login page if not logged in
+    header('Location: ../login.php'); // Redirect to the login page if not logged in
     exit();
 }
 
