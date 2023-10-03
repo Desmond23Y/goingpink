43a7ba037c9c5183f13dd9cb321f13ed239cb08c
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-03 10:06:29 +0800

    Update M_transport_homepage.php

--- TransportManagers/M_transport_homepage.php
+++ TransportManagers/M_transport_homepage.php
@@ -16,9 +16,53 @@
             <li><a href="../logout.php" class="right">LOGOUT</a></li>
         </ul>
     </nav>
+     <!-- Include Bootstrap-->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+     <!--Import Butler & Futura Font-->
+     <style>
+      @import url('https://fonts.cdnfonts.com/css/butler');
+      @import url('https://fonts.cdnfonts.com/css/futura-pt');
+     </style>
+    
+     <!--Javascript-->
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
-<h1> Transport Management Homepage </h1>
+  <div class="backgroun-image">
+            <header>
+                <h1>Welcome Back to the Transport Management Page!</h1>
+        
+                <p>What do you want to do today?</p>
+            </header>
+            <section class="section transport-database">
+                <div class="function" >
+                    <img src="/picture/transport-database.png" alt="transport" class="transport">
+                    <div class="function-text" id="transport-database">
+                        <h2 class="transport-database">Transport Database</h2>
+                        <p class="transport-database">View all the existing transport in the database.<br> You can edit and update transport details here too.  </p>
+                            <button class="button">View Transport Database</button>       
+                    </div>
+                </div>
+            </section>
+            <section class="section manage-transport-booking">
+                <div class="function" id="manage-transport-booking">
+                    <img src="/picture/transport-booking.png" style="float: right;" class="generate-report">
+                    <div class="function-text" >
+                        <h2>Manage Transport Bookings</h2>
+                        <p class="manage-booking">View, manage, confirm all of the transport related bookings here.</p>
+                        <button class="button">Manage Transport</button>
+                    </div>
+                </div>
+            </section>
+         
+            
+
+            
+        </div>
+
 </body>
 </html>
