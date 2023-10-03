4da35bcdc3d2ffb1f9dda9569bc86f07bc2a0859
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-10-03 13:50:52 +0800

    123

--- Supports/homepage_support.php
+++ Supports/homepage_support.php
@@ -30,7 +30,7 @@ session_start();
             <li><a href="support_report.php">REPORT</a></li>
             <li><a href="view_own_support_report.php?user_id=<?php echo $_SESSION['user_id']; ?>">VIEW OWN REPORT</a></li>
             <li><a href="view_ticket.php">VIEW TICKET</a></li>
-            <li><a href="logout.php" class="right">LOGOUT</a></li>
+            <li><a href="../logout.php" class="right">LOGOUT</a></li>
         </ul>
     </nav>
       <div class="backgroun-image">
