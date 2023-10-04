ad8c8958363c99e50628fcaa97634141f18614be
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-05 00:13:55 +0800

    Update M_edit_transport_info.php

--- TransportManagers/M_edit_transport_info.php
+++ TransportManagers/M_edit_transport_info.php
@@ -41,11 +41,11 @@ if (isset($_GET['transport_id'])) {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Edit Transport Information</title>
-            <link rel="stylesheet" href="M_navibar.css">
+            <link rel="stylesheet" href="M_transport_homepage.css">
              <nav>
                 <ul class="navibar">
                     <li><a href="M_transport_homepage.php">HOME</a></li>
-                    <li><a href="M_view_transport_info.php">TRANSPORT INFOROMATION</a></li>
+                    <li><a href="M_view_transport_info.php">TRANSPORT INFORMATION</a></li>
                     <li><a href="M_view_transport_booking.php">TRANSPORT BOOKING</a></li>
         
                     <li><a href="../logout.php" class="right">LOGOUT</a></li>
