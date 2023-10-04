048ea38081c7d312de60d686d51c285241e18bfa
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-04 16:29:30 +0800

    rating.php

--- Users/rating.php
+++ Users/rating.php
@@ -44,7 +44,7 @@ mysqli_close($con);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rating and Feedback</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="Rating.css">
 </head>
 <body>
     <header>
