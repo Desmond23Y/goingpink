69cefaa17784cfb3d96e056fb0f20bc6cf428971
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-10-04 16:44:58 +0800

    123

--- Users/view_transport_payment.php
+++ Users/view_transport_payment.php
@@ -1,35 +1,32 @@
 <?php
-// Start the session and include necessary files
 session_start();
 include('conn.php');
 
-// Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
     header('Location: ../login.php');
     exit();
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve POST data including the success flag
-    $success = isset($_POST['success']) ? $_POST['success'] : 'false';
+    // Retrieve data sent through AJAX
+    $userId = isset($_POST['user_id']) ? $_POST['user_id'] : '';
+    $transportId = isset($_POST['transport_id']) ? $_POST['transport_id'] : '';
+    $arrivalLocation = isset($_POST['arrival_location']) ? $_POST['arrival_location'] : '';
+    $departureLocation = isset($_POST['departure_location']) ? $_POST['departure_location'] : '';
+    $arrivalTime = isset($_POST['arrival_time']) ? $_POST['arrival_time'] : '';
+    $departureTime = isset($_POST['departure_time']) ? $_POST['departure_time'] : '';
+    $price = isset($_POST['price']) ? $_POST['price'] : '';
 
-    if ($success === 'true') {
-        // Payment was successful
-        echo "<h2>Payment Successful</h2>";
-        
-        // Retrieve and display the parameters
-        echo "User ID: " . $_SESSION['user_id'] . "<br>";
-        echo "Transport ID: " . $_SESSION['transport_id'] . "<br>";
-        echo "Arrival Location: " . $_POST['arrival_location'] . "<br>";
-        echo "Departure Location: " . $_POST['departure_location'] . "<br>";
-        echo "Arrival Time: " . $_POST['arrival_time'] . "<br>";
-        echo "Departure Time: " . $_POST['departure_time'] . "<br>";
-        echo "Price: " . $_POST['price'] . "<br>";
-    } else {
-        // Payment failed
-        $error = isset($_POST['error']) ? $_POST['error'] : 'An error occurred during payment.';
-        echo "Payment failed. Error: " . $error;
-    }
+    echo "<h2>Payment Successful</h2>";
+    
+    // Display the received data
+    echo "User ID: " . $userId . "<br>";
+    echo "Transport ID: " . $transportId . "<br>";
+    echo "Arrival Location: " . $arrivalLocation . "<br>";
+    echo "Departure Location: " . $departureLocation . "<br>";
+    echo "Arrival Time: " . $arrivalTime . "<br>";
+    echo "Departure Time: " . $departureTime . "<br>";
+    echo "Price: " . $price . "<br>";
 } else {
     // Invalid request
     echo "Invalid request.";
