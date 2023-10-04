48a2ef44204308bc88ed581b43626e128744ebb4
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-05 01:57:43 +0800

    Update view_rating.php

--- Users/view_rating.php
+++ Users/view_rating.php
@@ -36,6 +36,11 @@ if (!$result) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Ratings</title>
     <link rel="stylesheet" href="view_rating.css">
+    <style>
+     @import url('https://fonts.cdnfonts.com/css/butler');
+     @import url('https://fonts.cdnfonts.com/css/futura-pt');
+    </style>
+
 </head>
 <body>
     <header>
