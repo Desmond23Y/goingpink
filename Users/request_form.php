7676160b6dc6c5239d7039d03ed28b289330d8fe
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-04 15:31:02 +0800

    request_form.php

--- Users/request_form.php
+++ Users/request_form.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <head>
     <title>Customer Support Request Form</title>
-    <link rel="stylesheet" href="style_request_form.css">
+    <link rel="stylesheet" href="Requestform.css">
 </head>
 
 <body>
