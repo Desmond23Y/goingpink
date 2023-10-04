99d9d79d8f70cd2ad6c888216a1f53b51e9f50f6
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-10-04 16:26:01 +0800

    123123

--- Users/transportbooking.php
+++ Users/transportbooking.php
@@ -7,8 +7,7 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
-$user_id = $_SESSION['user_id'];
-$transport_id = $_SESSION['transport_id'];
+//include('../navi_bar.php');   #include this only when your user is accessing it as a GUI (if needed)
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve POST data including the price
@@ -53,8 +52,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         mysqli_stmt_bind_param($stmt, "ssssssssd", $transportManagerId, $userId, $adminId, $transportId, $arrivalLocation, $departureLocation, $arrivalTime, $departureTime, $price);
         if (mysqli_stmt_execute($stmt)) {
             mysqli_stmt_close($stmt);
-            echo json_encode(['success' => true]);
-            exit();
+
+            $_SESSION['user_id'] = $userId;
+            $_SESSION['transport_id'] = $transportId;
+
+            // Send an AJAX request to view_transport_payment.php
+            $.ajax({
+                type: "POST",
+                url: "view_transport_payment.php",
+                data: {
+                    success: true,
+                    user_id: '<?php echo $userId; ?>',
+                    transport_id: '<?php echo $transportId; ?>',
+                    arrival_location: '<?php echo $arrivalLocation; ?>',
+                    departure_location: '<?php echo $departureLocation; ?>',
+                    arrival_time: '<?php echo $arrivalTime; ?>',
+                    departure_time: '<?php echo $departureTime; ?>',
+                    price: '<?php echo $price; ?>'
+                },
+                success: function (response) {
+                    // Display the response to the user
+                    $("#booking-message").html(response);
+                },
+                error: function () {
+                    // Handle the error if the AJAX request fails
+                    $("#booking-message").html("An error occurred during payment.");
+                }
+            });
+            // No need to exit() here
         } else {
             mysqli_stmt_close($stmt);
             echo json_encode(['success' => false, 'error' => 'Database error: ' . mysqli_error($con)]);
@@ -69,3 +94,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     exit();
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hotel Booking</title>
+    <link rel="stylesheet" href="Hotelbooking.css">
+</head>
+<body>
+    <h2>Book a Hotel</h2>
+    <form id="hotel-booking-form" method="POST" action="">
+        <label for="number_of_pax">Number of Guests:</label>
+        <input type="number" id="number_of_pax" name="number_of_pax" min="1" required><br><br>
+
+        <label for="check_in_date">Check-In Date:</label>
+        <input type="date" id="check_in_date" name="check_in_date" required><br><br>
+
+        <label for="check_out_date">Check-Out Date:</
