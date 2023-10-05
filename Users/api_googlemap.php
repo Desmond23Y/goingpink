17221860f224a46bff50572f72cb546f14c90508
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-10-05 12:33:20 +0800

    mahai

--- Users/api_googlemap.php
+++ Users/api_googlemap.php
@@ -273,7 +273,7 @@ $_SESSION['transportpricing'] = $transportTypes;
 
                                     // Redirect to index.php after a delay (e.g., 2 seconds)
                                     setTimeout(function () {
-                                        window.location.href = './view_transport_payment.php?transport_id='+transportId+'&user_id='+<?=$_SESSION['user_id'];
+                                        window.location.href = './view_transport_payment.php?transport_id='+transportId+'&user_id='+<?=$_SESSION['user_id']?>;
                                     }, 2000);
                                 } else {
                                     alert('Booking failed');
