cf7fe5268f4b01f2951c10db0f5598c5ae8559d3
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-06 00:45:00 +0800

    1

--- Supports/view_own_support_report.php
+++ Supports/view_own_support_report.php
@@ -89,56 +89,7 @@ if (isset($_SESSION['user_id'])) {
     </table>
 </body>
 </html>
-
-    </style>
-
-</head>
-<body>
-     <nav>
-        <ul class="navibar">
-            <li><a href="homepage_support.php">HOME</a></li>
-            <li><a href="support_report.php">REPORT</a></li>
-            <li><a href="view_own_support_report.php">VIEW OWN REPORT</a></li>
-            <li><a href="view_ticket.php">VIEW TICKET</a></li>
-            <li><a href="../logout.php" class="right">LOGOUT</a></li>
-        </ul>
-    </nav>
-
-    <table width="90%">
-        <tr bgcolor="#E9204F">
-            <td>Report ID</td>
-            <td>Support ID</td>
-            <td>Report Title</td>
-            <td>Priority</td>
-            <td>Report Description</td>
-            <td>Report Status</td>
-            <td>Report Comment</td>
-        </tr>
-
-    <?php
-    if ($result !== null) {
-        while($row=mysqli_fetch_array($result))
-        {
-            echo'<tr>';
-            echo'<td>'.$row["report_id"].'</td>';
-            echo'<td>'.$row["support_id"].'</td>';
-            echo'<td>'.$row["report_title"].'</td>';
-            echo'<td>'.$row["priority"].'</td>';
-            echo'<td>'.$row["report_description"].'</td>';
-            echo'<td>'.$row["report_status"].'</td>';
-            echo'<td>'.$row["report_comment"].'</td>';
-            echo'</tr>';
-        }
-        mysqli_close($con);
-    } else {
-        echo "No records found.";
-    }
-    ?>
-    </table>
-    
-</body>
-</html>
-
+  
 
 
 
