0db0d0cb22d8d891f95c71586bc14c02c1410ea0
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-06 00:52:23 +0800

    1

--- Supports/view_ticket.php
+++ Supports/view_ticket.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 include("conn.php");
 
@@ -7,38 +7,114 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
-$result=mysqli_query($con,"SELECT* FROM ticket");
+$result = mysqli_query($con, "SELECT * FROM ticket");
 ?>
-<table width="90%">
-    <tr bgcolor="#FFB6C1">
-        <td>Ticket ID</td>
-        <td>Support ID</td>
-        <td>User ID</td>
-        <td>Contact Name</td>
-        <td>Support Type</td>
-        <td>Ticket Description</td>
-        <td>Ticket Status</td>
-        <td>Ticket Priority</td>
-        <td>Ticket Solution</td>
-        <td>Edit</td>
-    </tr>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>View Tickets</title>
+    <link rel="stylesheet" href="ticket.css">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
 
-<?php
-    while($row=mysqli_fetch_array($result))
-    {
-        echo'<tr>';
-        echo'<td>'.$row["ticket_id"].'</td>';
-        echo'<td>'.$row["support_id"].'</td>';
-        echo'<td>'.$row["user_id"].'</td>';
-        echo'<td>'.$row["contact_name"].'</td>';
-        echo'<td>'.$row["support_type"].'</td>';
-        echo'<td>'.$row["ticket_description"].'</td>';
-        echo'<td>'.$row["ticket_status"].'</td>';
-        echo'<td>'.$row["ticket_priority"].'</td>';
-        echo'<td>'.$row["ticket_solution"].'</td>';
-        echo'<td><a href="update_ticket.php?ticket_id='.$row["ticket_id"].'">Edit</a></td>';
-        echo'</tr>';
-    }
-    mysqli_close($con);
-?>
-</table>
+        .navibar {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #FFB6C1;
+            font-family: Butler, serif;
+        }
+
+        .navibar a {
+            float: left;
+            display: block;
+            color: #333;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        .navibar a.right {
+            float: right;
+        }
+
+        .navibar a:hover {
+            background-color: #C7949C;
+            color: white;
+        }
+
+        table {
+            width: 90%;
+            border-collapse: collapse;
+            margin: 20px auto; 
+        }
+
+        th, td {
+            border: 1px solid #FFB6C1;
+            padding: 8px;
+            text-align: left;
+        }
+
+        tr:nth-child(even) {
+            background-color: #FFE6E6;
+        }
+
+        tr:nth-child(odd) {
+            background-color: #FFF3F3;
+        }
+
+        th {
+            background-color: #FFB6C1;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <ul class="navibar">
+            <li><a href="homepage_support.php">HOME</a></li>
+            <li><a href="support_report.php">REPORT</a></li>
+            <li><a href="view_own_support_report.php">VIEW OWN REPORT</a></li>
+            <li><a href="view_ticket.php">VIEW TICKET</a></li>
+            <li><a href="../logout.php" class="right">LOGOUT</a></li>
+        </ul>
+    </nav>
+
+    <table>
+        <tr>
+            <th>Ticket ID</th>
+            <th>Support ID</th>
+            <th>User ID</th>
+            <th>Contact Name</th>
+            <th>Support Type</th>
+            <th>Ticket Description</th>
+            <th>Ticket Status</th>
+            <th>Ticket Priority</th>
+            <th>Ticket Solution</th>
+            <th>Edit</th>
+        </tr>
+
+        <?php
+        while ($row = mysqli_fetch_array($result)) {
+            echo '<tr>';
+            echo '<td>' . $row["ticket_id"] . '</td>';
+            echo '<td>' . $row["support_id"] . '</td>';
+            echo '<td>' . $row["user_id"] . '</td>';
+            echo '<td>' . $row["contact_name"] . '</td>';
+            echo '<td>' . $row["support_type"] . '</td>';
+            echo '<td>' . $row["ticket_description"] . '</td>';
+            echo '<td>' . $row["ticket_status"] . '</td>';
+            echo '<td>' . $row["ticket_priority"] . '</td>';
+            echo '<td>' . $row["ticket_solution"] . '</td>';
+            echo '<td><a href="update_ticket.php?ticket_id=' . $row["ticket_id"] . '">Edit</a></td>';
+            echo '</tr>';
+        }
+        mysqli_close($con);
+        ?>
+    </table>
+</body>
+</html>
