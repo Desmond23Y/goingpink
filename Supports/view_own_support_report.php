f38fb4faf769c8dc8e931a4ee492cd5e0bba6e06
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-06 00:33:50 +0800

    123

--- Supports/view_own_support_report.php
+++ Supports/view_own_support_report.php
@@ -11,35 +11,59 @@ if (isset($_SESSION['user_id'])) {
 }
 ?>
 
-<table width="90%">
-    <tr bgcolor="#FFB6C1">
-        <td>Report ID</td>
-        <td>Support ID</td>
-        <td>Report Title</td>
-        <td>Priority</td>
-        <td>Report Description</td>
-        <td>Report Status</td>
-        <td>Report Comment</td>
 
-    </tr>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>View Own Support Report</title>
+    <link rel="stylesheet" href="homepage_support.css">
+</head>
+<body>
+     <nav>
+        <ul class="navibar">
+            <li><a href="homepage_support.php">HOME</a></li>
+            <li><a href="support_report.php">REPORT</a></li>
+            <li><a href="view_own_support_report.php">VIEW OWN REPORT</a></li>
+            <li><a href="view_ticket.php">VIEW TICKET</a></li>
+            <li><a href="../logout.php" class="right">LOGOUT</a></li>
+        </ul>
+    </nav>
 
-<?php
-if ($result !== null) {
-    while($row=mysqli_fetch_array($result))
-    {
-        echo'<tr>';
-        echo'<td>'.$row["report_id"].'</td>';
-        echo'<td>'.$row["support_id"].'</td>';
-        echo'<td>'.$row["report_title"].'</td>';
-        echo'<td>'.$row["priority"].'</td>';
-        echo'<td>'.$row["report_description"].'</td>';
-        echo'<td>'.$row["report_status"].'</td>';
-        echo'<td>'.$row["report_comment"].'</td>';
-        echo'</tr>';
+    <table width="90%">
+        <tr bgcolor="#FFB6C1">
+            <td>Report ID</td>
+            <td>Support ID</td>
+            <td>Report Title</td>
+            <td>Priority</td>
+            <td>Report Description</td>
+            <td>Report Status</td>
+            <td>Report Comment</td>
+        </tr>
+
+    <?php
+    if ($result !== null) {
+        while($row=mysqli_fetch_array($result))
+        {
+            echo'<tr>';
+            echo'<td>'.$row["report_id"].'</td>';
+            echo'<td>'.$row["support_id"].'</td>';
+            echo'<td>'.$row["report_title"].'</td>';
+            echo'<td>'.$row["priority"].'</td>';
+            echo'<td>'.$row["report_description"].'</td>';
+            echo'<td>'.$row["report_status"].'</td>';
+            echo'<td>'.$row["report_comment"].'</td>';
+            echo'</tr>';
+        }
+        mysqli_close($con);
+    } else {
+        echo "No records found.";
     }
-    mysqli_close($con);
-} else {
-    echo "No records found.";
-}
-?>
-</table>
+    ?>
+    </table>
+    
+</body>
+</html>
+
+
+
+
