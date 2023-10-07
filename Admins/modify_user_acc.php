f8e1b3d2351109b148eda67999469be4cb48eb04
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-07 22:47:51 +0800

    modify_user_acc.php

--- Admins/modify_user_acc.php
+++ Admins/modify_user_acc.php
@@ -101,8 +101,10 @@ if (isset($_GET['user_id'])) {
                 <input type="radio"name="gender"<?php if ($row['gender'] == "Female") { ?> checked="checked" <?php } ?> value="Female"required="required">Female
                 <br><br>
 
-                <button type="submit" value="submit">Edit User Account</button>
-            </form>
+            </form   
+                
+            ><button type="submit" value="submit">Edit User Account</button>
+            
         <?php
         }
         mysqli_close($con);
