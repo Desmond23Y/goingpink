097895143b2c2b201a6bce59246a3362f6907e5a
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-08 22:40:54 +0800

    Navi_bar_admin.php

---
+++ Admins/Navi_bar_admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="navibaradmin.css">
+</head>
+<body>
+    <nav>
+        <ul class="navibar">
+            <li><a href="homepage_admin.php">HOME</a></li>
+            <li><a href="../logout.php" class="right">LOGOUT</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
