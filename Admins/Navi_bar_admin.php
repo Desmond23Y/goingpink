42517e89612a9d830a3d639fa2beea56bb2a6de2
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-10 07:02:36 +0800

    Navi_bar_admin.php

--- Admins/Navi_bar_admin.php
+++ Admins/Navi_bar_admin.php
@@ -2,86 +2,65 @@
 <html>
 <style>
 body {
+    font-family: Arial, sans-serif;
     margin: 0;
-    padding: 0;
-    }
+}
 
 .navibar {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
+    background-color: #333;
     overflow: hidden;
-    background-color: #F9B9C3;
-    font-family: Butler, serif;
-    font-size: 15px;
-    }
+}
 
-.navibar a {
+.navibar ul {
     list-style-type: none;
     margin: 0;
     padding: 0;
-    }
+}
 
-.navibar a {
+.navibar li {
     float: left;
-    }
+}
 
-.navibar a.right {
-    float: right;
-    }
-
-.navibar a, .dropdown-content a {
+.navibar li a, .dropdown-content a {
     display: block;
     color: white;
     text-align: center;
     padding: 14px 16px;
     text-decoration: none;
-    }
+}
 
-.navibar li a:hover, .dropdown:hover .dropbtn {
+.navbar li a:hover, .dropdown:hover .dropbtn {
     background-color: #ddd;
     color: black;
-    }
+}
 
 .dropdown {
     display: inline-block;
-    }
+}
 
 .dropdown-content {
     display: none;
     position: absolute;
     background-color: #f9f9f9;
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     z-index: 1;
-    }      
+}
 
 .dropdown-content a:hover {
     background-color: #ddd;
     color: black;
-    }
+}
 
 .dropdown:hover .dropdown-content {
     display: block;
-    }
-
+}
 </style>
 
 <body>
     <nav>
         <ul class="navibar">
             <li><a href="homepage_admin.php">HOME</a></li>
-            <li class="dropdown">
-                <a class="dropbtn">MANAGE</a>
-                <div class="dropdown-content">
-                    <a href="view_user_acc.php">USER</a>
-                    <a href="view_hmgt_acc.php">HOTEL</a>
-                    <a href="view_tmgt_acc.php">TRANSPORT</a>
-                    <a href="view_support_acc.php">SUPPORT</a>
-                    <a href="view_admin_acc.php">ADMIN</a>
-                </div>
-            </li>
-            <li><a href="generatereport.php">GENERATE REPORT</a></li>
-            <li><a href="view_hotel_booking.php">MANAGE HOTEL BOOKING</a></li>
-            <li><a href="view_transport_booking.php">MANAGE TRANSPORT BOOKING</a></li>
+            <li><a href="view_hotel_booking.php">BACK</a></li>
             <li><a href="../logout.php" class="right">LOGOUT</a></li>
         </ul>
     </nav>
