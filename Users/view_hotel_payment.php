82730e1aceb6956f614eb6b634e48f038d597cf3
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-10-09 16:48:07 +0800

    123

--- Users/view_hotel_payment.php
+++ Users/view_hotel_payment.php
@@ -8,29 +8,22 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 $user_id = $_SESSION['user_id'];
-$hotel_id = $_SESSION['selected_hotel_id'];
 
-$hotel_query = "SELECT hotel_id, hotel_total_price 
-               FROM hotel_booking 
-               WHERE user_id = '$user_id'
-               ORDER BY hotel_id DESC LIMIT 1";
-$hotel_result = mysqli_query($con, $hotel_query);
-if (!$hotel_result) {
+$latest_booking_query = "SELECT hb.*, hi.hotel_name, hi.room_type, hi.hotel_price, u.username, u.phone_number, u.email
+                         FROM hotel_booking hb
+                         JOIN user u ON hb.user_id = u.user_id
+                         JOIN hotel_information hi ON hb.hotel_id = hi.hotel_id
+                         WHERE hb.user_id = '$user_id'
+                         ORDER BY hb.hotel_booking_id DESC
+                         LIMIT 1";
+
+$latest_booking_result = mysqli_query($con, $latest_booking_query);
+
+if (!$latest_booking_result) {
     die('Query Error: ' . mysqli_error($con));
 }
 
-if (isset($_GET['hotel_id']) && isset($_GET['user_id'])) {
-    $hotel_id = $_GET['hotel_id'];
-
-    $result = mysqli_query($con, "SELECT * FROM hotel_booking WHERE hotel_id = '$hotel_id' AND user_id = '$user_id'");
-
-    $result2 = mysqli_query($con, "SELECT * FROM user WHERE user_id = '$user_id'");
-
-    $result3 = mysqli_query($con, "SELECT * FROM hotel_information WHERE hotel_id = '$hotel_id'");
-    if (!$result || !$result2 || !$result3) {
-        die('Query Error: ' . mysqli_error($con));
-    }
-}
+$row = mysqli_fetch_assoc($latest_booking_result);
 ?>
 
 <html>
@@ -45,23 +38,19 @@ if (isset($_GET['hotel_id']) && isset($_GET['user_id'])) {
 <h1>Hotel Charges</h1>
 <div class="box">
     <?php
-    if (isset($result) && isset($result2) && isset($result3)) {
-        if (mysqli_num_rows($result) > 0 && mysqli_num_rows($result2) > 0 && mysqli_num_rows($result3) > 0) {
-            $row1 = mysqli_fetch_assoc($result);
-            $row2 = mysqli_fetch_assoc($result2);
-            $row3 = mysqli_fetch_assoc($result3);
-
-            echo '<h3> Username: '. $row2["username"] . '</h3>';
-            echo '<h3> Phone number: ' . $row2["phone_number"] . '</h3>';
-            echo '<h3> Email: ' . $row2["email"] . '</h3>';
-            echo '<h3> Booking ID: ' . $row1["hotel_booking_id"] . '</h3>';
-            echo '<h3> Hotel Name: ' . $row3["hotel_name"] . '</h3>';
-            echo '<h3> Room type: ' . $row3["room_type"] . '</h3>';
-            echo '<h3> Check in Date: ' . $row1["check_in_date"] . '</h3>';
-            echo '<h3> Check out Date: ' . $row1["check_out_date"] . '</h3>';
-            echo '<h3> Number of Guests: ' . $row1["number_of_pax"] . '</h3>';
-            echo '<h3> Hotel Price: ' . $row3["hotel_price"] . '</h3>';
-        }
+    if ($row) {
+        echo '<h3> Username: '. $row["username"] . '</h3>';
+        echo '<h3> Phone number: ' . $row["phone_number"] . '</h3>';
+        echo '<h3> Email: ' . $row["email"] . '</h3>';
+        echo '<h3> Booking ID: ' . $row["hotel_booking_id"] . '</h3>';
+        echo '<h3> Hotel Name: ' . $row["hotel_name"] . '</h3>';
+        echo '<h3> Room type: ' . $row["room_type"] . '</h3>';
+        echo '<h3> Check in Date: ' . $row["check_in_date"] . '</h3>';
+        echo '<h3> Check out Date: ' . $row["check_out_date"] . '</h3>';
+        echo '<h3> Number of Guests: ' . $row["number_of_pax"] . '</h3>';
+        echo '<h3> Hotel Price: ' . $row["hotel_price"] . '</h3>';
+    } else {
+        echo '<p>No hotel booking found.</p>';
     }
     ?>
 </div>
