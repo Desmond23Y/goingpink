75e02da2c9ce0044c948ba3391d7dc168a49ad90
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-09 14:18:52 +0800

    report_ticket_type.php

--- Admins/report_ticket_type.php
+++ Admins/report_ticket_type.php
@@ -30,7 +30,7 @@
     // Close the database connection
     mysqli_close($con);
 
-    include("Navi_bar_admin.php");
+    include("Navi_generate_report.php");
     ?>
 
     <h2>Support Type Statistics</h2>
