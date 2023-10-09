9b3dbf28f7556f4c17a4cd0eedb31e87c488dbed
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-09 16:59:55 +0800

    Navi_modify_support.php

---
+++ Admins/Navi_modify_support.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<style>
+    body {
+    margin: 0;
+    padding: 0;
+    }
+
+.navibar {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #F9B9C3;
+    font-family: Butler, serif;
+    font-size: 15px;
+    }
+
+.navibar a {
+    float: left;
+    display: block;
+    color: #333;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    }
+
+.navibar a.right {
+    float: right;
+    }
+
+.navibar a:hover {
+    background-color: #C7949C;
+    color: white;
+    }
+
+.logo {
+    display: inline-block;
+    vertical-align: middle;
+    width: 40px;
+    height: auto; 
+    margin-right: 10px; 
+    }
+</style>
+
+<body>
+    <nav>
+        <ul class="navibar">
+            <li><a href="homepage_admin.php">HOME</a></li>
+            <li><a href="view_support_acc.php">BACK</a></li>
+            <li><a href="../logout.php" class="right">LOGOUT</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
