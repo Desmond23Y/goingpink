09a876fd3fb5444e6b9ae220d4b459459f276b97
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-10 06:48:25 +0800

    Navi_bar_admin.php

--- Admins/Navi_bar_admin.php
+++ Admins/Navi_bar_admin.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <style>
-    body {
+body {
     margin: 0;
     padding: 0;
     }
@@ -16,45 +16,53 @@
     font-size: 15px;
     }
 
-.navibar a{
+.navibar ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    }
+
+.navibar li {
     float: left;
-    display: block;
-    color: #333;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
     }
 
 .navibar a.right {
     float: right;
     }
 
-.dropdown-content {
-    display: none;
-    position: absolute;
-    z-index: 1;
-}
-
-.navibar a:hover, .dropdown:hover .dropbtn,.dropdown-content a:hover {
-    background-color: #C7949C;
+.navibar li a, .dropdown-content a {
+    display: block;
     color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    }
+
+.navibar li a:hover, .dropdown:hover .dropbtn {
+    background-color: #ddd;
+    color: black;
     }
 
 .dropdown {
-    display: inline-block   
+    display: inline-block;
     }
 
-.dropdown:hover, .dropdown-content {
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    z-index: 1;
+    }      
+
+.dropdown-content a:hover {
+    background-color: #ddd;
+    color: black;
+    }
+
+.dropdown:hover .dropdown-content {
     display: block;
     }
 
-.logo {
-    display: inline-block;
-    vertical-align: middle;
-    width: 40px;
-    height: auto; 
-    margin-right: 10px; 
-    }
 </style>
 
 <body>
