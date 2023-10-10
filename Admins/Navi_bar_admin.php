d5d34d6e7eb1e0a67b268c76918d70c72753fe2e
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-10 08:29:36 +0800

    Navi_bar_admin.php

--- Admins/Navi_bar_admin.php
+++ Admins/Navi_bar_admin.php
@@ -2,85 +2,65 @@
 <html>
 <style>
 body {
-    font-family: Arial, sans-serif;
-    margin: 0;
     padding: 0;
-    }   
-
-nav {
-    background-color: #333;
-    }
-
-.navibar {
-    list-style-type: none;
     margin: 0;
-    padding: 0;
-    overflow: hidden;
+    font-family: 'Butler';
     }
 
-.navibar li {
-    float: left;
+ul {
+    list-style: none;
+    background-color: #F9B9C3;
     }
 
-.navibar li a, .dropbtn {
+ul li {
     display: inline-block;
-    color: white;
+    position: relative;
+    }
+
+ul li a {
+    display: block;
+    padding: 20px 25px;
+    color: #333;
+    text-decoration: none;
     text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
+    font-size: 15px;
     }
 
-.navibar li a:hover, .dropdown:hover .dropbtn {
-    background-color: #555;
+ul li ul.dropdown-content li {
+    display: block;
     }
 
-.dropdown {
-    display: inline-block;
-    }
-
-.dropdown-content {
-    display: none;
+ul li ul.dropdown-content {
+    width: 100%;
+    background-color: #F9B9C3;
     position: absolute;
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    z-index: 1;
+    z-index: 999;
+    display: none;
     }
 
-.dropdown-content a {
-    color: black;
-    padding: 12px 16px;
+ul li a:hover {
+    background-color: #C7949C;
+    color: white;
+    }
+
+ul li:hover ul.dropdown-content {
     display: block;
-    text-decoration: none;
-    text-align: left;
-    }
-
-.dropdown-content a:hover {
-    background-color: #ddd;
-    }
-
-.dropdown:hover .dropdown-content {
-    display: block;
-    }
-
-.right {
-    float: right;
-    }
+}
 </style>
 
 <body>
     <nav>
         <ul class="navibar">
             <li><a href="homepage_admin.php">HOME</a></li>
-            <li class="dropdown">
-                <a class="dropbtn">MANAGE</a>
-                <div class="dropdown-content">
+            <li>
+                <a class="dropdown">MANAGE</a>
+                <ul class="dropdown-content">
                     <a href="view_user_acc.php">USER</a>
                     <a href="view_hmgt_acc.php">HOTEL</a>
                     <a href="view_tmgt_acc.php">TRANSPORT</a>
                     <a href="view_support_acc.php">SUPPORT</a>
                     <a href="view_admin_acc.php">ADMIN</a>
-                </div>
+                </ul>
             </li>
             <li><a href="generatereport.php">GENERATE REPORT</a></li>
             <li><a href="view_hotel_booking.php">MANAGE HOTEL BOOKING</a></li>
