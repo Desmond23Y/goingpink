0db4b9812fbff917d4a159b96f88d03a9918fcc3
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-11 14:24:49 +0800

    create_hmgt_acc.php

--- Admins/create_hmgt_acc.php
+++ Admins/create_hmgt_acc.php
@@ -21,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="createhmgtacc.css">
+    <link rel="stylesheet" href="Createhmgtacc.css">
 </head>
 
 <body>
