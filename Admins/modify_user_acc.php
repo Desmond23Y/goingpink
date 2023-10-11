26bed4517bb19bc2245ae654e8c136c558d3adab
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-11 12:24:45 +0800

    modify_user_acc.php

--- Admins/modify_user_acc.php
+++ Admins/modify_user_acc.php
@@ -65,7 +65,7 @@ if (isset($_GET['user_id'])) {
         <html>
         <head>
             <title>Modify User Account</title>
-            <link rel="stylesheet" href="modifyhmgtacc.css">
+            <link rel="stylesheet" href="modifyuseracc.css">
         </head>
         <body>
             <h2>User Account Modification</h2>
