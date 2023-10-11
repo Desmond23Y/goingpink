7273f29e7aa80cded37280c73fa94cc59a450426
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-11 13:54:37 +0800

    modify_tmgt_acc.php

--- Admins/modify_tmgt_acc.php
+++ Admins/modify_tmgt_acc.php
@@ -49,7 +49,7 @@ if (isset($_GET['transport_manager_id'])) {
         <html>
         <head>
             <title>Modify Transport Manager Account</title>
-            <link rel="stylesheet" href="modifyhmgtacc.css">
+            <link rel="stylesheet" href="Modifyhmgtacc.css">
         </head>
         <body>
             <h2>Transport Manager Account Modification</h2>
