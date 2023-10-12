e930a396c12246520dda4b5c917c849e26559558
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-13 01:30:20 +0800

    support_report.php

--- Supports/support_report.php
+++ Supports/support_report.php
@@ -29,7 +29,6 @@ if (!isset($_SESSION['user_id'])) {
 <head>
      <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
         <link rel="stylesheet" href="supportreport.css">
     
         <!-- Include Bootstrap-->
