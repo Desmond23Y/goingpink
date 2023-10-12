e206c7e30c5af5c8d498fd5deb473c171a96edc5
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-12 17:43:01 +0800

    homepage_admin.php

--- Admins/homepage_admin.php
+++ Admins/homepage_admin.php
@@ -7,7 +7,7 @@ include("Navi_bar_admin.php");
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>GoingPink | Better Travel Site</title>
-            <link rel="stylesheet" href="homepage_admin.css">
+            <link rel="stylesheet" href="Homepage_admin.css">
             <!-- Include Bootstrap-->
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
