19b8f212e1ee6ad67af7e2115f448602f6f49837
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-13 01:21:30 +0800

    support_report.php

--- Supports/support_report.php
+++ Supports/support_report.php
@@ -39,7 +39,40 @@ if (!isset($_SESSION['user_id'])) {
         <style>
         @import url('https://fonts.cdnfonts.com/css/butler');
         @import url('https://fonts.cdnfonts.com/css/futura-pt');
+
+        body {
+        margin: 0;
+        padding: 0;
+        }
+
+        .navibar {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #F9B9C3;
+        }
+
+        .navibar a {
+        float: left;
+        display: block;
+        color: #333;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        }
+        
+        .navibar a.right {
+        float: right;
+        }
+        
+        .navibar a:hover {
+        background-color: #C7949C;
+        color: white;
+        }
+            
         </style>
+    
     <title>Report to Admin</title>
 </head>
 
@@ -47,6 +80,16 @@ if (!isset($_SESSION['user_id'])) {
     <header>
         <h1>Report Form</h1>
     </header>
+
+    <nav>
+    <ul class="navibar">
+        <li><a href="homepage_support.php">HOME</a></li>
+        <li><a href="support_report.php">REPORT</a></li>
+        <li><a href="view_own_support_report.php">VIEW OWN REPORT</a></li>
+        <li><a href="view_ticket.php">VIEW TICKET</a></li>
+        <li><a href="../logout.php" class="right">LOGOUT</a></li>
+    </ul>
+    </nav>
     
     <form method="post">
     <label for="title">Title:</label>
