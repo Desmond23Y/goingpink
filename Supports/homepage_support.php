2989394eb34dd5fbbc67b744a91e6a237eb48bc4
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-13 01:37:53 +0800

    homepage_support.php

--- Supports/homepage_support.php
+++ Supports/homepage_support.php
@@ -20,9 +20,42 @@ session_start();
        <!--Javascript-->
        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-    
-    </head>
 
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            }
+
+        .navibar {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #F9B9C3;
+            }
+
+        .navibar a {
+            float: left;
+            display: block;
+            color: #333;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            }
+        
+        .navibar a.right {
+            float: right;
+            }
+        
+        .navibar a:hover {
+            background-color: #C7949C;
+            color: white;
+            }
+        
+    </style>
+    </head>
+     
 <body>
     <nav>
         <ul class="navibar">
