4ff8ed572d5d6444be142738729908975843d20d
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-12 16:26:34 +0800

    homepage_admin.php

--- Admins/homepage_admin.php
+++ Admins/homepage_admin.php
@@ -1,34 +1,33 @@
 <?php
 include("Navi_bar_admin.php");
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>GoingPink | Better Travel Site</title>
-        <link rel="stylesheet" href="homepage_admin.css">
-        <!-- Include Bootstrap-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>GoingPink | Better Travel Site</title>
+            <link rel="stylesheet" href="homepage_admin.css">
+            <!-- Include Bootstrap-->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
-        <!--Import Butler & Futura Font-->
-        <style>
-        @import url('https://fonts.cdnfonts.com/css/butler');
-        @import url('https://fonts.cdnfonts.com/css/futura-pt');
-        </style>
+            <!--Import Butler & Futura Font-->
+            <style>
+            @import url('https://fonts.cdnfonts.com/css/butler');
+            @import url('https://fonts.cdnfonts.com/css/futura-pt');
+            </style>
     
-       <!--Javascript-->
-       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+           <!--Javascript-->
+           <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
   
-    </head>
+        </head>
 
-<body>
+    <body>
  
        <div class="background-image">
             <header>
                 <h1>Admin Dashboard</h1>
-        
                 <p>What do you want to do today?</p>
             </header>
             <section class="section manage-account">
@@ -40,15 +39,15 @@ include("Navi_bar_admin.php");
                         <p class="manage-account">Manage all of your stakeholder's account in one place.</p>
                      
                         
-                        <div class="button-container">
+                            <div class="button-container">
                               <a href="view_user_acc.php" ><button class="button">User</button></a>
                               <a href="view_hmgt_acc.php"><button class="button">Hotel </button></a>
                               <a href="view_tmgt_acc.php"><button class="button">Transport</button></a>
                               <a href="view_support_acc.php" ><button class="button">Support</button></a>
                               <a href="view_admin_acc.php"><button class="button">Admin</button></a>
+                            </div>
                         </div>
                     </div>
-                </div>
             </section>
             <section class="section generate-report">
                 <div class="function" id="generate-report">
