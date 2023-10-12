3f1695f446bd15fc78d3f8296dd4ce1cf2b59c34
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-13 01:11:31 +0800

    support_report.php

--- Supports/support_report.php
+++ Supports/support_report.php
@@ -43,16 +43,6 @@ if (!isset($_SESSION['user_id'])) {
     <title>Report to Admin</title>
 </head>
 
-<nav>
-    <ul class="navibar">
-        <li><a href="homepage_support.php">HOME</a></li>
-        <li><a href="support_report.php">REPORT</a></li>
-        <li><a href="view_own_support_report.php">VIEW OWN REPORT</a></li>
-        <li><a href="view_ticket.php">VIEW TICKET</a></li>
-        <li><a href="../logout.php" class="right">LOGOUT</a></li>
-    </ul>
-</nav>
-
 <body>
     <header>
         <h1>Report Form</h1>
