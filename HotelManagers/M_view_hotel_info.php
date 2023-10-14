669d2210fa422b0dacf615a46e3bd78fa242a8be
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-14 23:26:49 +0800

    1

--- HotelManagers/M_view_hotel_info.php
+++ HotelManagers/M_view_hotel_info.php
@@ -15,7 +15,7 @@ if (!$result) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel Information</title>
-    <link rel="stylesheet" href="M_view_hotel_info.css">
+    <link rel="stylesheet" href="mviewhotelinfo.css">
     <nav>
         <ul class="navibar">
             <li><a href="M_hotel_homepage.php">HOME</a></li>
