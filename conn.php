dbff02377f08a345b686588d19990f13f097031b
Jeslynk12 <115882788+Jeslynk12@users.noreply.github.com>
2023-10-15 19:53:18 +0000

    try and error

--- conn.php
+++ conn.php
@@ -2,6 +2,9 @@
 $con = mysqli_connect("localhost","id21215862_goingpink","@Goingpink123","id21215862_goingpink");
 
 if ($con->connect_error) {
-    die("Could not connect to the server!: " . $con->connect_error);
+    $con = mysqli_connect("fdb1034.awardspace.net","4388183_goingpink","@Goingpink123","4388183_goingpink");
+    if ($con->connect_error) {
+        die("Could not connect to the server!: " . $con->connect_error);
+    }
 }
 ?>
