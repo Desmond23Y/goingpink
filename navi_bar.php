8d7d7182dcbd5848c9d2ef2b71d58aa846c80f19
hiimfloo <129931147+hiimfloo@users.noreply.github.com>
2023-10-15 17:34:52 +0800

    1

--- navi_bar.php
+++ navi_bar.php
@@ -4,7 +4,7 @@
 body {
     margin: 0;
     padding: 0;
-}
+    }
 
 .navibar {
     list-style-type: none;
@@ -12,55 +12,92 @@ body {
     padding: 0;
     overflow: hidden;
     background-color: #F9B9C3;
-    font-family: Butler, serif;
-}
+    }
 
 .navibar a {
     float: left;
-    display: block;
+    font-size: 16px;
     color: #333;
     text-align: center;
     padding: 14px 16px;
     text-decoration: none;
-}
+    }
 
 .navibar a.right {
-    float: right;
-}
+    float: right;   
+    }
 
 .navibar a:hover {
     background-color: #C7949C;
     color: white;
-}
+    }
 
-.logo {
-    display: inline-block;
-    vertical-align: middle;
-    width: 40px;
-    height: auto; 
-    margin-right: 10px; 
-}
+.dropdown {
+    float: left;
+    overflow: hidden;
+    position: static;
+    }
 
+.dropdown .dropbtn {
+    font-size: 16px;
+    border: none;
+    outline: none;
+    color: #333;
+    padding: 14px 16px;
+    background-color: #F9B9C3;
+    }
+    
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #F9B9C3;
+    min-width: 160px;
+    z-index: 1;
+    }    
+
+.dropdown-content a {
+    float: none;
+    color: #333;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: center;
+    }
+
+.dropdown:hover .dropdown-content {
+    display: block;
+    }
 
 
 </style>
 
 <body>
-        <nav>
-            <ul class="navibar">
-                <li><a href="../index.php">HOME</a></li>
-                <li><a href="../Users/view_profile.php">PROFILE</a></li>
-                <li><a href="../Users/faq.php">FAQ</a></li>
-                <li><a href="../Users/invoice.php">CHECK BOOKING HISTORY</a></li>
+      
+            <div class="navibar">
+                <a href="../index.php">HOME</a>
+                <a href="../Users/view_profile.php">PROFILE</a>
+                <a href="../Users/faq.php">FAQ</a>
+                    <div class="dropdown" style="font-family: Butler;">
+                        <button class="dropbtn" style="font-family: Butler;">FUNCTIONS
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="../Users/viewhotel.php">VIEW AND BOOK HOTEL</a>
+                            <a href="../Users/api_exRates.php">CURRENCY EXCHANGE RATES</a>
+                            <a href="../Users/viewtransport.php">VIEW AND BOOK TRANSPORT</a>                 
+                            <a href="../Users/invoice.php">CHECK BOOKING HISTORY</a>
 
-               
+                            <a href="../Users/request_form.php">GO TO SUPPORT</a>
+                            <a href="../Users/user_view_ticket.php">VIEW CREATED TICKETS</a>
 
-                
-                
-                <li><a href="../logout.php" class="right">LOGOUT</a></li>
-                <li><a href="../register.php" class="right">REGISTER</a></li>            
-                <li><a href="../login.php" class="right">LOGIN</a></li>
-            </ul>
-        </nav>
+                            <a href="../Users/rating.php">WRITE REVIEW</a>
+                            <a href="../Users/view_rating.php">VIEW REVIEW</a>
+                        </div>
+                    </div>              
+                <a href="../logout.php" class="right">LOGOUT</a>
+                <a href="../register.php" class="right">REGISTER</a>
+                <a href="../login.php" class="right">LOGIN</a>
+            </div>
+       
 </body>
 </html>
