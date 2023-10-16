5873b74b62b9e128ebd8e0c8a042c45104912f8f
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-16 08:40:07 +0800

    Navi_bar_admin.php

--- Admins/Navi_bar_admin.php
+++ Admins/Navi_bar_admin.php
@@ -3,72 +3,72 @@
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
-    body {
-        margin: 0;
-        padding: 0;
-        }
-    
-    .navibar {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: #F9B9C3;
-        }
-    
-    .navibar a {
-        float: left;
-        font-size: 16px;
-        color: #333;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        }
-    
-    .navibar a.right {
-        float: right;   
-        }
-    
-    .navibar a:hover {
-        background-color: #C7949C;
-        color: white;
-        }
-    
-    .dropdown {
-        float: left;
-        overflow: hidden;
-        position: static;
-        }
-    
-    .dropdown .dropbtn {
-        font-size: 16px;
-        border: none;
-        outline: none;
-        color: #333;
-        padding: 14px 16px;
-        background-color: #F9B9C3;
-        }
+        body {
+            margin: 0;
+            padding: 0;
+            }
         
-    .dropdown-content {
-        display: none;
-        position: absolute;
-        background-color: #F9B9C3;
-        min-width: 160px;
-        z-index: 2;
-        }    
-    
-    .dropdown-content a {
-        float: none;
-        color: #333;
-        padding: 12px 16px;
-        text-decoration: none;
-        display: block;
-        text-align: center;
-        }
-    
-    .dropdown:hover .dropdown-content {
-        display: block;
-        }
+        .navibar {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #F9B9C3;
+            }
+        
+        .navibar a {
+            float: left;
+            font-size: 16px;
+            color: #333;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            }
+        
+        .navibar a.right {
+            float: right;   
+            }
+        
+        .navibar a:hover {
+            background-color: #C7949C;
+            color: white;
+            }
+        
+        .dropdown {
+            float: left;
+            overflow: hidden;
+            position: static;
+            }
+        
+        .dropdown .dropbtn {
+            font-size: 16px;
+            border: none;
+            outline: none;
+            color: #333;
+            padding: 14px 16px;
+            background-color: #F9B9C3;
+            }
+            
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #F9B9C3;
+            min-width: 160px;
+            z-index: 2;
+            }    
+        
+        .dropdown-content a {
+            float: none;
+            color: #333;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: center;
+            }
+        
+        .dropdown:hover .dropdown-content {
+            display: block;
+            }
 </style>
 </head>
 
