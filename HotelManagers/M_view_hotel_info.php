7a94c07b463e65e29a502f33f5d6242e76d406c1
Desmond <142880728+Desmond23Y@users.noreply.github.com>
2023-10-19 18:45:58 +0800

    Update M_view_hotel_info.php

--- HotelManagers/M_view_hotel_info.php
+++ HotelManagers/M_view_hotel_info.php
@@ -45,9 +45,9 @@ if (!$result) {
               
 
                  // Check for type of car and display the image accordingly
-                 if ($row["hotel_name"] == "Secret Recipe Hotel") {
+                if ($row["hotel_name"] == "Secret View Hotel") {
                     echo '<img src="../picture/hotel/secretrecipehotel.jpg" alt="sr-hotel">';
-                } elseif ($row["hotel_name"] == "Northern Nugget Resort") {
+                } elseif ($row["hotel_name"] == "Northern Resort") {
                     echo '<img src="../picture/hotel/nn.jpg" alt="nn-hotel">';
                 } elseif ($row["hotel_name"] == "Prophecy Hotel") {
                     echo '<img src="../picture/hotel/prophecy.jpg" alt="p-hotel">';
@@ -59,11 +59,13 @@ if (!$result) {
                     echo '<img src="../picture/hotel/ct.jpg" alt="ct-hotel">';
                 } elseif ($row["hotel_name"] == "Vanilla Hotel & Spa") {
                     echo '<img src="../picture/hotel/vhs.jpg" alt="vhs-hotel">';
-                }elseif ($row["hotel_name"] == "DES") {
+                }elseif ($row["hotel_name"] == "Coast Hotel") {
                     echo '<img src="../picture/hotel/d1.jpg" alt="d1-hotel">';
-                } elseif ($row["hotel_name"] == "des") {
+                } elseif ($row["hotel_name"] == "Elegant Relax Hotel") {
                     echo '<img src="../picture/hotel/d2.jpg" alt="d2-hotel">';
-                } 
+                } else {
+                     echo '<img src="../picture/hotel/defaulthotel.jpg" alt="default-hotel">';
+                }
               
                 echo '<h3> Hotel Name:  <span class="thin-font"> ' . $row["hotel_name"] . '</span></h3>';
                 echo '<h3> Room Type:   <span class="thin-font">' . $row["room_type"] . '</span></h3>';
