6ae85ec63492a3e090b5243b19f1024586ef1ac6
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-10-23 17:03:40 +0800

    123

--- forgotpass.php
+++ forgotpass.php
@@ -5,12 +5,12 @@
     <link rel="stylesheet" href="../Admins/Modifyhmgtacc.css">
 </head>
 <body>
-    <h1>Enter Your Gmail Address</h1>
+    <h2>Enter Your Gmail Address</h2>
     <form method="post" action="process.php">
         <label for="email">Gmail Address:</label>
         <input type="email" name="email" id="email" required>
         <br>
-        <input type="submit" value="Submit">
+        <button type="submit">Submit</button>
     </form>
 </body>
 </html>
