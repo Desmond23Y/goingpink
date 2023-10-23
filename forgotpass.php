59c91e09c3d4970387b5b67488c1cfa5dce8ce5d
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-10-23 17:13:51 +0800

    123

--- forgotpass.php
+++ forgotpass.php
@@ -5,8 +5,24 @@
     <link rel="stylesheet" href="../Admins/Modifyhmgtacc.css">
 </head>
 <body>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Check if the form was submitted
+        if (isset($_POST['email']) && !empty($_POST['email'])) {
+            // Display a confirmation message
+            echo "Gmail submitted. You will be redirected to the index page in a moment...";
+            
+            // Redirect to index.php after a delay
+            header('Refresh: 5; URL=index.php'); // Redirect after 5 seconds
+            exit();
+        } else {
+            // Handle any validation errors or other logic here
+            echo "Please enter a valid Gmail address.";
+        }
+    }
+    ?>
     <h2>Enter Your Gmail Address</h2>
-    <form method="post" action="process.php">
+    <form method="post" action="">
         <label for="email">Gmail Address:</label>
         <input type="email" name="email" id="email" required>
         <br>
@@ -14,3 +30,4 @@
     </form>
 </body>
 </html>
+
