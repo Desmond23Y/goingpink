6d90f4d7d983f022034a56a4b0207da56bd2d89b
SheldonLee19 <143935711+SheldonLee19@users.noreply.github.com>
2023-10-26 01:25:52 +0800

    view_profile.php

--- Users/view_profile.php
+++ Users/view_profile.php
@@ -88,7 +88,7 @@ include('../navi_bar.php');
         <label for="female">Female</label><br>
 
         <br><br> 
-        <a href="edit_profile.php" class="button">Edit Profile</a>
+        <button href="edit_profile.php">Edit Profile</button>
         </form>
     </div>
 </body>
