c8211618ebbff3dc127cbae890b8802474a8ddaf
KennyAPU <129933735+KennyAPU@users.noreply.github.com>
2023-10-25 15:38:23 +0800

    123

--- Users/edit_profile.php
+++ Users/edit_profile.php
@@ -110,7 +110,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <br><br>
 
         <label for="new_password">New Password (Leave blank to keep current password):</label>
-        <input type="password" id="new_password" name="new_password" value="<?php echo $password; ?>">
+        <input type="text" id="new_password" name="new_password" value="<?php echo $password; ?>">
         <br><br>
 
         <label for="first_name">First Name:</label>
